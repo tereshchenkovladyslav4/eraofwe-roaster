232a38053095f6ce1ecb7afcf27cafe857027bb7
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2020-06-15 00:02:28 +0530

    Health Checked Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { HealthCheckComponent } from './health-check/health-check.component';
 
 
 export const routes: Routes = [
+  { path: "health-check", component: HealthCheckComponent},
   {
     path: 'features',
     loadChildren: () => import('./features/features.module')
