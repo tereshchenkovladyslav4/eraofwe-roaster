6586be6c14ee7226c07e5ea2304f8180961003de
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2020-12-01 16:23:14 +0530

    roaster ui fixes

--- src/app/features/Farm Link/coffee-experience/default-setting/default-setting.component.ts
+++ src/app/features/Farm Link/coffee-experience/default-setting/default-setting.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalsService } from 'src/services/globals.service';
 
 @Component({
   selector: 'app-default-setting',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DefaultSettingComponent implements OnInit {
 
-  constructor() { }
+  date1: Date;
+  appLanguage?: any;
+	coffeeDetailsActive:any = 0;
+
+  constructor(public globals: GlobalsService) { }
 
   ngOnInit(): void {
+    this.language();
+
+    $("body").on("change", ".custom-label__inputs", function () {
+      var $this = $(this);
+  
+      if ($this.is(':checked')) {
+          $this.parents('.custom-label').addClass('active');
+          var totalChecked = $('.roastery-tags__checkboxes').find('.custom-label.active').length;
+          console.log(totalChecked)
+  
+          if (totalChecked == 5) {
+              $('.roastery-tags__checkboxes').find('.custom-label').find('.custom-label__inputs').prop('disabled', true);
+              $('.roastery-tags__checkboxes').find('.custom-label.active').find('.custom-label__inputs').prop('disabled', false);
+          }
+      }
+  
+      else {
+          $this.parents('.custom-label').removeClass('active');
+          $('.roastery-tags__checkboxes').find('.custom-label').find('.custom-label__inputs').prop('disabled', false);
+          $('.roastery-tags__checkboxes').find('.custom-label.active').find('.custom-label__inputs').prop('disabled', false);
+      }
+  });
+
+  }
+
+  language(){
+    this.appLanguage = this.globals.languageJson;
+    this.coffeeDetailsActive++;
   }
 
 }
