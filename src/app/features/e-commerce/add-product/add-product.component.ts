1089af6b5124a82fbffe4991ea3577eb9a6cca9b
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2020-12-15 16:07:37 +0530

    catlog

--- src/app/features/e-commerce/add-product/add-product.component.ts
+++ src/app/features/e-commerce/add-product/add-product.component.ts
@@ -8,11 +8,50 @@ import { GlobalsService } from 'src/services/globals.service';
 })
 export class AddProductComponent implements OnInit {
   appLanguage?: any;
+  btnToggle = true;
+  statusChange: string;
+  activeFlag: boolean = true;
+
+  grindVariant = [
+    {
+      price : '',
+      grind : '',
+      stock : '',
+      skunumber : ""
+    }
+  ]
 
   constructor(public globals: GlobalsService) { }
 
   ngOnInit(): void {
     this.appLanguage = this.globals.languageJson;
   }
+
+  activeStatus() {
+    this.btnToggle = !this.btnToggle;
+    if(this.btnToggle == true){
+      this.statusChange = "ACTIVE";
+      console.log(this.statusChange)
+    }
+    else{
+      this.statusChange = "INACTIVE";
+      console.log(this.statusChange)
+    }
+  }
+
+  public addNewVariant(){
+    this.grindVariant.push({ 
+    price : '',
+    grind : '',
+    stock : '',
+    skunumber : ""
+
+    });
+}
+
+public deleteRow( index){
+    this.grindVariant.splice(index, 1);
+  }
+
 
 }
