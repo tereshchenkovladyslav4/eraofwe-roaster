6331096974860951795729c7c66c6c635a41045a
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2020-12-30 21:31:22 +0300

    multi period sensoring

--- src/app/ordermanagement/remote-sensoring/weather-chart/weather-chart.component.ts
+++ src/app/ordermanagement/remote-sensoring/weather-chart/weather-chart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GlobalsService } from 'src/services/globals.service';
 import { AgroService } from 'src/services/agro.service';
 import * as moment from 'moment';
+import * as _ from 'underscore';
 import { HttpErrorResponse } from '@angular/common/http';
 declare var $: any;
 
@@ -11,12 +12,27 @@ declare var $: any;
   styleUrls: ['./weather-chart.component.scss'],
 })
 export class WeatherChartComponent implements OnInit {
+  readonly WeatherConst = {
+    0: 'temp',
+    1: 'wind',
+    2: 'rain',
+    3: 'clouds',
+    4: 'pressure',
+    5: 'humidity',
+  };
+  dateKeyStrings = ['YYYY/MM/DD/HH', 'YYYY/MM/DD', 'YYYY/MM/DD', 'YYYY/MM/DD'];
+
   appLanguage?: any;
+  chartData1: any[] = [];
+  chartData2: any[] = [];
+  legends: any[] = [];
+  showLegend = false;
   weatherTypes: any[] = [
     {
       value: 0,
       label: 'Temperature',
       title: 'Temperature(°C)',
+      unit: '°C',
       minimum: 0,
       maximum: 30,
       interval: 5,
@@ -25,6 +41,7 @@ export class WeatherChartComponent implements OnInit {
       value: 1,
       label: 'Wind',
       title: 'Wind Speed(m/s)',
+      unit: 'm/s',
       minimum: 0,
       maximum: 10,
       interval: 2,
@@ -33,6 +50,7 @@ export class WeatherChartComponent implements OnInit {
       value: 2,
       label: 'Rainfall',
       title: 'Rainfall(mm)',
+      unit: 'mm',
       minimum: 0,
       maximum: 75,
       interval: 15,
@@ -41,6 +59,7 @@ export class WeatherChartComponent implements OnInit {
       value: 3,
       label: 'Cloudiness',
       title: 'Cloudiness(%)',
+      unit: '%',
       minimum: 0,
       maximum: 100,
       interval: 20,
@@ -49,6 +68,7 @@ export class WeatherChartComponent implements OnInit {
       value: 4,
       label: 'Pressure',
       title: 'Pressure(hPa)',
+      unit: 'hPa',
       minimum: 1000,
       maximum: 1050,
       interval: 10,
@@ -57,26 +77,65 @@ export class WeatherChartComponent implements OnInit {
       value: 5,
       label: 'Humidity',
       title: 'Humidity(%)',
+      unit: '%',
       minimum: 0,
       maximum: 100,
       interval: 20,
     },
   ];
   selWeatherType = 0;
-  periods: any[] = [
-    { value: 0, label: 'Hourly forecast', labelFormat: 'h', intervalType: 'Hours' },
-    { value: 1, label: 'Last 2 weeks', labelFormat: 'd', intervalType: 'Days' },
-    { value: 2, label: 'Last 1 month', labelFormat: 'd', intervalType: 'Days' },
-    { value: 3, label: 'Historical', labelFormat: 'y', intervalType: 'Years' },
+  periods: any[];
+  periodsForAll: any[] = [
+    {
+      value: 0,
+      label: 'Hourly forecast',
+      labelFormat: 'h',
+      intervalType: 'Hours',
+      period: 'daily',
+    },
+    {
+      value: 1,
+      label: 'Last 2 weeks',
+      labelFormat: 'd',
+      intervalType: 'Days',
+      period: 'twoWeeks',
+    },
+    {
+      value: 2,
+      label: 'Last 1 month',
+      labelFormat: 'd',
+      intervalType: 'Days',
+      period: 'month',
+    },
+    {
+      value: 3,
+      label: 'Historical',
+      labelFormat: 'd MMM',
+      intervalType: 'Months',
+      period: 'range',
+    },
+  ];
+  periodsForRain: any[] = [
+    {
+      value: 0,
+      label: 'Daily rain volume',
+      labelFormat: 'h',
+      intervalType: 'Hours',
+      period: 'daily',
+    },
+    {
+      value: 1,
+      label: 'Accumulated rainfall',
+      labelFormat: 'y',
+      intervalType: 'Years',
+      period: 'range',
+    },
   ];
   selPeriod = 0;
-
-  public data: any[] = [];
+  dateFormats = ['DD MMM, LT', 'DD MMM', 'DD MMM', 'DD MMM'];
 
   public primaryXAxis = {
     valueType: 'DateTime',
-    labelFormat: 'h',
-    intervalType: 'Hours',
     interval: 1,
     edgeLabelPlacement: 'Shift',
     majorGridLines: { width: 0 },
@@ -85,15 +144,12 @@ export class WeatherChartComponent implements OnInit {
   public primaryYAxis = {
     labelFormat: '{value}',
     rangePadding: 'None',
-    minimum: 0,
-    maximum: 30,
-    interval: 5,
     lineStyle: { width: 0 },
+    majorGridLines: { dashArray: '7,5' },
     majorTickLines: { width: 0 },
     minorTickLines: { width: 0 },
-    title: this.weatherTypes[this.selWeatherType].title,
     titleStyle: {
-      size: '20px',
+      size: '16px',
       color: '#232334',
       fontFamily: 'Muli',
       fontWeight: '500',
@@ -104,23 +160,32 @@ export class WeatherChartComponent implements OnInit {
       width: 0,
     },
   };
-  public width = '100%';
+  palette = ['#2DAEA8', '#0D6B67'];
   public marker = {
     visible: true,
     height: 10,
     width: 10,
   };
-  public tooltip = {
+  public tooltip: any = {
     enable: true,
+    fill: '#fff',
+    border: {
+      width: 0,
+    },
+    textStyle: {
+      color: '#747588',
+    },
+    format: '${point.tooltip}',
   };
   legendSettings = { visible: false };
   selectedDate = new Date('2020-12-26');
-  weatherData: any[];
+  weatherData: any[] = [];
 
   constructor(public globals: GlobalsService, public agroSrv: AgroService) {}
 
   ngOnInit(): void {
-    this.getHistoricalWeather();
+    this.changeWeatherType(0);
+    this.changePeriod(0);
   }
 
   changeWeatherType(value) {
@@ -129,6 +194,13 @@ export class WeatherChartComponent implements OnInit {
       ...this.primaryYAxis,
       ...this.weatherTypes[this.selWeatherType],
     };
+    if (value === 2) {
+      this.periods = this.periodsForRain;
+      this.selPeriod = 0;
+    } else {
+      this.periods = this.periodsForAll;
+    }
+    this.updateChartSetting();
     this.makeData();
   }
 
@@ -138,46 +210,60 @@ export class WeatherChartComponent implements OnInit {
       ...this.primaryXAxis,
       ...this.periods[this.selPeriod],
     };
+    this.updateChartSetting();
     this.getHistoricalWeather();
+  }
+
+  updateChartSetting() {
+    if (
+      this.selWeatherType === 0 &&
+      (this.selPeriod === 1 || this.selPeriod === 2)
+    ) {
+      // Legend setting of which has two lines
+      this.showLegend = true;
+      this.legends = [{ label: 'Minimum' }, { label: 'Maximum' }];
+    } else {
+      // Legend setting of which has one line
+      this.showLegend = false;
+      this.legends = [{ label: this.weatherTypes[this.selWeatherType].label }];
+    }
   }
 
   getHistoricalWeather() {
     let query;
-    switch (this.selPeriod) {
-      case 0: {
+    switch (this.periods[this.selPeriod].period) {
+      case 'daily': {
         query = {
           start: moment(this.selectedDate).startOf('day').unix(),
           end: moment(this.selectedDate).endOf('day').unix(),
         };
         break;
       }
-      case 1: {
+      case 'twoWeeks': {
         query = {
           start: moment().startOf('day').subtract(2, 'weeks').unix(),
           end: moment().startOf('day').subtract(1, 'hours').unix(),
         };
         break;
       }
-      case 2: {
+      case 'month': {
         query = {
           start: moment().startOf('day').subtract(1, 'months').unix(),
           end: moment().startOf('day').subtract(1, 'hours').unix(),
         };
         break;
       }
-      case 2: {
+      default: {
         query = {
-          start: moment().startOf('day').subtract(1, 'months').unix(),
+          start: moment().startOf('day').subtract(3, 'months').unix(),
           end: moment().startOf('day').subtract(1, 'hours').unix(),
         };
-        break;
       }
     }
     this.agroSrv.getHistoricalWeather(query).subscribe(
       (res: any) => {
-        console.log('Weather:', res);
         this.weatherData = res;
-        this.makeData();
+        this.processData();
       },
       (err: HttpErrorResponse) => {
         console.log(err);
@@ -185,40 +271,141 @@ export class WeatherChartComponent implements OnInit {
     );
   }
 
+  processData() {
+    const self = this;
+    this.weatherData = _.chain(this.weatherData)
+      .map((item) => {
+        let key;
+        key = moment.unix(item.dt).format(self.dateKeyStrings[self.selPeriod]);
+        return {
+          key,
+          data: item,
+          tempMin: item.main.temp_min,
+          tempMax: item.main.temp_max,
+        };
+      })
+      .groupBy('key')
+      .map((value, key) => {
+        const dt = value[0].data.dt;
+        const temp =
+          _.reduce(
+            value,
+            (acc, val) => {
+              return acc + (val.data?.main?.temp || 0);
+            },
+            0
+          ) / (value.length || 1);
+        const tempMin = _.min(_.pluck(value, 'tempMin'));
+        const tempMax = _.max(_.pluck(value, 'tempMax'));
+        const wind =
+          _.reduce(
+            value,
+            (acc, val) => {
+              return acc + (val.data?.wind?.speed || 0);
+            },
+            0
+          ) / (value.length || 1);
+        const rain =
+          _.reduce(
+            value,
+            (acc, val) => {
+              return acc + ((val.data.rain && val.data.rain['3h']) || 0);
+            },
+            0
+          ) / (value.length || 1);
+        const clouds =
+          _.reduce(
+            value,
+            (acc, val) => {
+              return acc + (val.data.clouds?.all || 0);
+            },
+            0
+          ) / (value.length || 1);
+        const pressure =
+          _.reduce(
+            value,
+            (acc, val) => {
+              return acc + (val.data.main?.pressure || 0);
+            },
+            0
+          ) / (value.length || 1);
+        const humidity =
+          _.reduce(
+            value,
+            (acc, val) => {
+              return acc + (val.data.main?.humidity || 0);
+            },
+            0
+          ) / (value.length || 1);
+        return {
+          key,
+          dt,
+          temp,
+          tempMin,
+          tempMax,
+          wind,
+          rain,
+          clouds,
+          pressure,
+          humidity,
+        };
+      })
+      .value();
+    this.makeData();
+  }
+
   makeData() {
-    this.data = [];
+    const tempData1 = [];
+    const tempData2 = [];
     this.weatherData.forEach((element, index) => {
-      let y;
+      let y = null;
+      let y1 = null;
       switch (this.selWeatherType) {
         case 0: {
-          y = element.main.temp - 273.15;
+          if (this.selPeriod === 0 || this.selPeriod === 3) {
+            y = (element.temp - 273.15).toFixed(1);
+          } else {
+            y = (element.tempMin - 273.15).toFixed(1);
+            y1 = (element.tempMax - 273.15).toFixed(1);
+          }
           break;
         }
-        case 1: {
-          y = element.wind.speed;
-          break;
-        }
-        case 2: {
-          y = (element.rain && element.rain['3h']) || 0;
-          break;
-        }
-        case 3: {
-          y = element.clouds.all;
-          break;
-        }
-        case 4: {
-          y = element.main.pressure;
-          break;
-        }
-        case 5: {
-          y = element.main.humidity;
-          break;
+        default: {
+          y = element[this.WeatherConst[this.selWeatherType]].toFixed(1);
         }
       }
-      this.data.push({
+      const label = `${moment
+        .unix(element.dt)
+        .format(this.dateFormats[this.selPeriod])}
+        <br /><strong>
+        ${
+          this.legends[0].label +
+          ' ' +
+          y +
+          this.weatherTypes[this.selWeatherType].unit
+        }
+        ${
+          this.legends[1]
+            ? ' ' +
+              this.legends[1].label +
+              ' ' +
+              y1 +
+              this.weatherTypes[this.selWeatherType].unit
+            : ''
+        }</strong>`;
+
+      tempData1.push({
         x: moment.unix(element.dt).toDate(),
         y,
+        label,
+      });
+      tempData2.push({
+        x: moment.unix(element.dt).toDate(),
+        y: y1,
+        label,
       });
     });
+    this.chartData1 = tempData1;
+    this.chartData2 = tempData2;
   }
 }
