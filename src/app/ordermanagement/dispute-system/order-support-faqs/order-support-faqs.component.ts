7e836e08df4b528e789897699c9b7eb9ec7857ae
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-01-11 16:18:10 +0530

    Integrated API for FAQ GET call

--- src/app/ordermanagement/dispute-system/order-support-faqs/order-support-faqs.component.ts
+++ src/app/ordermanagement/dispute-system/order-support-faqs/order-support-faqs.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { UserserviceService } from 'src/services/users/userservice.service';
 
 @Component({
   selector: 'app-order-support-faqs',
@@ -9,15 +11,34 @@ import { ActivatedRoute } from '@angular/router';
 export class OrderSupportFaqsComponent implements OnInit {
   headerValue: any;
   orderID: string = '';
-  constructor(private route: ActivatedRoute) { }
+  roaster_id: string = "";
+  questionDetails: any = [];
+  constructor(private route: ActivatedRoute, public userService: UserserviceService,
+    public cookieService: CookieService) { }
 
   ngOnInit(): void {
+    this.roaster_id = this.cookieService.get('roaster_id');
     this.orderID = decodeURIComponent(this.route.snapshot.queryParams['id']);
     if (this.route.snapshot.queryParams["buttonValue"]) {
       this.headerValue = decodeURIComponent(
         this.route.snapshot.queryParams["buttonValue"]
       );
     }
+    this.getFAQList();
+  }
+  getFAQList() {
+    this.userService.getDisputeFAQList(this.roaster_id).subscribe(res => {
+      console.log(res);
+      if (res['success'] && res['result']) {
+        console.log(res['result']);
+        this.questionDetails = res['result'].map((ele, index) => {
+          ele['element_id'] = 'collapseOne' + index;
+          return ele;
+        }).filter(item => item['status'] == 'enabled');
+      }
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
