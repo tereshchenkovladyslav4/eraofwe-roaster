ef1e1eb88553c173868aecf2f233ffa19f34ef97
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-01-12 19:20:05 +0530

    sourcing UI

--- src/app/features/Farm Link/brand-profile/brand-profile.component.ts
+++ src/app/features/Farm Link/brand-profile/brand-profile.component.ts
@@ -8,24 +8,39 @@ import { GlobalsService } from 'src/services/globals.service';
   styleUrls: ['./brand-profile.component.css']
 })
 export class BrandProfileComponent implements OnInit {
-  homeval: number = 5 ;
-  aboutval: number = 15 ;
-  learnval:  number = 5 ;
-  sustainval: number = 5 ;
-  visitval: number = 15 ;
+  homeval: number = 5;
+  aboutval: number = 15;
+  learnval: number = 5;
+  sustainval: number = 5;
+  visitval: number = 15;
   appLanguage?: any;
-	brandProfileActive:any=0;
-  constructor(
-		public globals: GlobalsService) { }
+  brandProfileActive: any = 0;
+  btnToggle = true;
+  statusChange: string;
+  activeFlag: boolean = true;
+  editmode: boolean = false;
 
-ngOnInit(): void {
+  constructor(
+    public globals: GlobalsService) { }
+
+  ngOnInit(): void {
     this.language();
     $('.nav-links__item').removeClass('active');
     $('.nav-links__item').eq(1).addClass('active');
-}
-language(){
-	this.appLanguage = this.globals.languageJson;
-	this.brandProfileActive++;
-}
+  }
+  language() {
+    this.appLanguage = this.globals.languageJson;
+    this.brandProfileActive++;
+  }
+
+  activeStatus() {
+    this.btnToggle = !this.btnToggle;
+    if (this.btnToggle == true) {
+      this.statusChange = "ACTIVE";
+    }
+    else {
+      this.statusChange = "INACTIVE";
+    }
+  }
 
 }
