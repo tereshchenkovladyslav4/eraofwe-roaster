330e3d248ba27a8b18933607d08d34f5f89f5b4a
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-01-13 19:18:04 +0300

    real image in sensoring

--- src/services/agro.service.ts
+++ src/services/agro.service.ts
@@ -3,58 +3,62 @@ import { Injectable } from '@angular/core';
 import * as moment from 'moment';
 
 @Injectable({
-  providedIn: 'root',
+    providedIn: 'root',
 })
 export class AgroService {
-  appid = '593c1b38da7aca9aca05192c565760b4';
-  apiUrl = 'https://api.agromonitoring.com/agro/1.0/';
+    appid = '593c1b38da7aca9aca05192c565760b4';
+    apiUrl = 'https://api.agromonitoring.com/agro/1.0/';
 
-  constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {}
 
-  checkTime(query: any) {
-    // Check time
-    if (query.end <= query.start) {
-      const temp = query.start;
-      query.start = query.end;
-      query.end = temp;
+    checkTime(query: any) {
+        // Check time
+        if (query.end <= query.start) {
+            const temp = query.start;
+            query.start = query.end;
+            query.end = temp;
+        }
+        query.sart = Math.min(query.start, moment().subtract(1, 'hours').unix());
+        query.end = Math.min(query.end, moment().subtract(1, 'hours').unix());
+        return query;
     }
-    query.sart = Math.min(query.start, moment().subtract(1, 'hours').unix());
-    query.end = Math.min(query.end, moment().subtract(1, 'hours').unix());
-    return query;
-  }
 
-  getHistoricalWeather(polyid, query: any = {}) {
-    query = this.checkTime(query);
-    return this.http.get(
-      `${this.apiUrl}weather/history?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
-    );
-  }
+    getPloygon(polyid) {
+        return this.http.get(`${this.apiUrl}polygons/${polyid}?appid=${this.appid}`);
+    }
 
-  getHistoricalSoil(polyid, query: any = {}) {
-    query = this.checkTime(query);
-    return this.http.get(
-      `${this.apiUrl}soil/history?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
-    );
-  }
+    getHistoricalWeather(polyid, query: any = {}) {
+        query = this.checkTime(query);
+        return this.http.get(
+            `${this.apiUrl}weather/history?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
+        );
+    }
 
-  getHistoricalUv(polyid, query: any = {}) {
-    query = this.checkTime(query);
-    return this.http.get(
-      `${this.apiUrl}uvi/history?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
-    );
-  }
+    getHistoricalSoil(polyid, query: any = {}) {
+        query = this.checkTime(query);
+        return this.http.get(
+            `${this.apiUrl}soil/history?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
+        );
+    }
 
-  getHistoricalNdvi(polyid, query: any = {}) {
-    query = this.checkTime(query);
-    return this.http.get(
-      `${this.apiUrl}ndvi/history?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
-    );
-  }
+    getHistoricalUv(polyid, query: any = {}) {
+        query = this.checkTime(query);
+        return this.http.get(
+            `${this.apiUrl}uvi/history?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
+        );
+    }
 
-  getImage(polyid, query: any = {}) {
-    query = this.checkTime(query);
-    return this.http.get(
-      `${this.apiUrl}image/search?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
-    );
-  }
+    getHistoricalNdvi(polyid, query: any = {}) {
+        query = this.checkTime(query);
+        return this.http.get(
+            `${this.apiUrl}ndvi/history?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
+        );
+    }
+
+    getImage(polyid, query: any = {}) {
+        query = this.checkTime(query);
+        return this.http.get(
+            `${this.apiUrl}image/search?appid=${this.appid}&polyid=${polyid}&start=${query.start}&end=${query.end}`
+        );
+    }
 }
