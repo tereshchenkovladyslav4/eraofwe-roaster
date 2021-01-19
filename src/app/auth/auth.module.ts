b88125da23d0ea43da4e7c589cbba8baed471787
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-01-19 22:10:13 +0300

    checkbox in sign up

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,21 +1,24 @@
-import { CommonModule } from "@angular/common";
-import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ImageCropperModule } from "ngx-image-cropper";
-import { DialogModule } from "primeng/dialog";
-import { SliderModule } from "primeng/slider";
-import { AuthRoutingModule } from "./auth-routing.module";
-import { AuthComponent } from "./auth.component";
-import { ChangePasswordComponent } from "./change-password/change-password.component";
-import { ForgotPasswordComponent } from "./forgot-password/forgot-password.component";
-import { LoginComponent } from "./login/login.component";
-import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
-import { PrivacyPolicyComponent } from "./privacy-policy/privacy-policy.component";
-import { LicenseComponent } from "./setup/license/license.component";
-import { ProfileImageComponent } from "./setup/profile-image/profile-image.component";
-import { SetupComponent } from "./setup/setup.component";
-import { UpdatePasswordComponent } from "./update-password/update-password.component";
-import { VerifyOtpComponent } from "./verify-otp/verify-otp.component";
+
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { ImageCropperModule } from 'ngx-image-cropper';
+import { DialogModule } from 'primeng/dialog';
+import { SliderModule } from 'primeng/slider';
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth.component';
+import { ChangePasswordComponent } from './change-password/change-password.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { LicenseComponent } from './setup/license/license.component';
+import { ProfileImageComponent } from './setup/profile-image/profile-image.component';
+import { SetupComponent } from './setup/setup.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
+import { VerifyOtpComponent } from './verify-otp/verify-otp.component';
 import { RoasterProfileComponent } from './setup/roaster-profile/roaster-profile.component';
 
 import { CookieService } from 'ngx-cookie-service';
@@ -27,34 +30,34 @@ import { ErrorModuleModule } from '../error-module/error-module.module';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    ForgotPasswordComponent,
-    AuthComponent,
-    PagenotfoundComponent,
-    VerifyOtpComponent,
-    ChangePasswordComponent,
-    SetupComponent,
-    UpdatePasswordComponent,
-    PrivacyPolicyComponent,
-    LicenseComponent,
-    ProfileImageComponent,
-    RoasterProfileComponent,
-    SignUpComponent,
- 
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    DialogModule,
-    ImageCropperModule,
-    SliderModule,
-    FormsModule,
-    HttpClientModule,
-    NgOtpInputModule,
-    ErrorModuleModule,
-    ToastrModule.forRoot({ timeOut: 10000, preventDuplicates: true })
-  ],
-  providers: [CookieService, UserserviceService],
+    declarations: [
+        LoginComponent,
+        ForgotPasswordComponent,
+        AuthComponent,
+        PagenotfoundComponent,
+        VerifyOtpComponent,
+        ChangePasswordComponent,
+        SetupComponent,
+        UpdatePasswordComponent,
+        PrivacyPolicyComponent,
+        LicenseComponent,
+        ProfileImageComponent,
+        RoasterProfileComponent,
+        SignUpComponent,
+    ],
+    imports: [
+        CommonModule,
+        AuthRoutingModule,
+        DialogModule,
+        ImageCropperModule,
+        SliderModule,
+        FormsModule,
+        HttpClientModule,
+        NgOtpInputModule,
+        ErrorModuleModule,
+        ToastrModule.forRoot({ timeOut: 10000, preventDuplicates: true }),
+        SharedModule,
+    ],
+    providers: [CookieService, UserserviceService],
 })
-export class AuthModule { }
+export class AuthModule {}
