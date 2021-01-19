5ebb9510301c8412926decdcb8bbcc1b3c28d5d1
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-01-19 13:58:51 +0300

    remove header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { PeopleModule } from './people/people.module';
 import { HealthCheckComponent } from './health-check/health-check.component';
 import { CalendarModule } from 'primeng/calendar';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { HeaderComponent } from './header/header.component';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { GalleriaModule } from 'primeng/galleria';
 import { MatVideoModule } from 'mat-video';
@@ -29,65 +28,65 @@ import { UserserviceService } from 'src/services/users/userservice.service';
 import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, HealthCheckComponent, HeaderComponent, LayoutComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule,
-    AuthModule,
-    FeaturesModule,
-    PeopleModule,
-    OverlayPanelModule,
-    CalendarModule,
-    PopoverModule,
-    OrdermanagementModule,
-    GalleriaModule,
-    MatVideoModule,
-    ErrorModuleModule,
-    HttpClientModule,
-    ToastrModule.forRoot({ timeOut: 10000, preventDuplicates: true, positionClass: 'toast-bottom-right' }),
-    AnimateOnScrollModule.forRoot(),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-  ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: ErrorIntercept,
-      multi: true,
-    },
-    {
-      provide: APP_INITIALIZER,
-      useFactory: ConfigLoader,
-      multi: true,
-      deps: [UserserviceService]
-    },
-    AuthGuard,
-  ],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HealthCheckComponent, LayoutComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        AuthModule,
+        FeaturesModule,
+        PeopleModule,
+        OverlayPanelModule,
+        CalendarModule,
+        PopoverModule,
+        OrdermanagementModule,
+        GalleriaModule,
+        MatVideoModule,
+        ErrorModuleModule,
+        HttpClientModule,
+        ToastrModule.forRoot({ timeOut: 10000, preventDuplicates: true, positionClass: 'toast-bottom-right' }),
+        AnimateOnScrollModule.forRoot(),
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ],
+    providers: [
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: ErrorIntercept,
+            multi: true,
+        },
+        {
+            provide: APP_INITIALIZER,
+            useFactory: ConfigLoader,
+            multi: true,
+            deps: [UserserviceService],
+        },
+        AuthGuard,
+    ],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
-export function ConfigLoader(
-  userService: UserserviceService,
-  cookieService: CookieService
-) {
-  return () => {
-    const queryParams = new URLSearchParams(window.location.search);
-    // Setting token
-    if (userService.cookieService.get('Auth') && userService.cookieService.get('roaster_id')) {
-      return Promise.resolve().then(() => {
-        const promise2 = userService.getUserPermissionPromise(userService.cookieService.get('roaster_id'));
-        promise2.then(response => {
-          if (response && response['success'] == true) {
-            const permissionList = response['result'];
-            userService.cookieService.set('permissionSlug', JSON.stringify(permissionList));
-          }
-          return response;
-        }, err => {
-          return err;
-        });
-        return promise2;
-      });
-    }
-    return;
-  };
+export class AppModule {}
+export function ConfigLoader(userService: UserserviceService, cookieService: CookieService) {
+    return () => {
+        const queryParams = new URLSearchParams(window.location.search);
+        // Setting token
+        if (userService.cookieService.get('Auth') && userService.cookieService.get('roaster_id')) {
+            return Promise.resolve().then(() => {
+                const promise2 = userService.getUserPermissionPromise(userService.cookieService.get('roaster_id'));
+                promise2.then(
+                    (response) => {
+                        if (response && response['success'] == true) {
+                            const permissionList = response['result'];
+                            userService.cookieService.set('permissionSlug', JSON.stringify(permissionList));
+                        }
+                        return response;
+                    },
+                    (err) => {
+                        return err;
+                    },
+                );
+                return promise2;
+            });
+        }
+        return;
+    };
 }
