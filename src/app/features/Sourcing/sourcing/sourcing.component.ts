636bd97168c0afc1e1147d70ea742e51051946cf
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-01-21 12:16:26 +0300

    add primeNG dropdown style

--- src/app/features/Sourcing/sourcing/sourcing.component.ts
+++ src/app/features/Sourcing/sourcing/sourcing.component.ts
@@ -48,6 +48,13 @@ export class SourcingComponent implements OnInit {
     filterCoffee: any = null;
 
     menuItems: any[];
+    gradeItems: any[] = [
+        { label: 'All', value: null },
+        { label: '81.0 - 83.0', value: '81.0,83.0' },
+        { label: '84.0 - 86.0', value: '84.0,86.0' },
+        { label: '87.0 - 89.0', value: '87.0,89.0' },
+        { label: '90+', value: '90' },
+    ];
 
     queryParams: any = {
         origin: '',
