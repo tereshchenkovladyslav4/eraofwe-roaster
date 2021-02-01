dd3f74ce4fe2f76d658e9469856a456b69f1acab
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-02-02 01:04:44 +0300

    brand profile design

--- src/app/modules/brand-profile/brand-profile.component.ts
+++ src/app/modules/brand-profile/brand-profile.component.ts
@@ -4,37 +4,50 @@ import { GlobalsService } from 'src/services/globals.service';
 @Component({
     selector: 'app-brand-profile',
     templateUrl: './brand-profile.component.html',
-    styleUrls: ['./brand-profile.component.css'],
+    styleUrls: ['./brand-profile.component.scss'],
 })
 export class BrandProfileComponent implements OnInit {
-    homeval: number = 5;
-    aboutval: number = 15;
-    learnval: number = 5;
-    sustainval: number = 5;
-    visitval: number = 15;
-    appLanguage?: any;
-    brandProfileActive: any = 0;
-    btnToggle = true;
-    statusChange: string;
-    activeFlag: boolean = true;
-    editmode: boolean = false;
+    breadItems: any[];
+    pages: any[] = [
+        {
+            title: 'Home Page',
+            desp: 'This is your company’s main landing page, use interesting content to bring in your user',
+            slug: 'home-page',
+        },
+        {
+            title: 'About Us',
+            desp: 'Write about your origin, who you are and what makes your company special',
+            slug: 'about-us',
+        },
+        {
+            title: 'Learn',
+            desp: 'Share facts about your coffee vairety, lots coffee making techinques',
+            slug: 'learn',
+        },
+        {
+            title: 'Sustainability',
+            desp: 'Tell you users about your social responsibility and sustainability practices',
+            slug: 'sustainability',
+        },
+        {
+            title: 'Blog',
+            desp: 'Add images and videos of your estate to give the user a whole experience',
+            slug: 'blog',
+        },
+        {
+            title: 'Visit Us',
+            desp: 'Add your contact details and basic FAQs of your estate',
+            slug: 'visit-us',
+        },
+    ];
 
     constructor(public globals: GlobalsService) {}
 
     ngOnInit(): void {
-        this.language();
-    }
-    language() {
-        this.appLanguage = this.globals.languageJson;
-        this.brandProfileActive++;
-    }
-
-    activeStatus() {
-        this.btnToggle = !this.btnToggle;
-        if (this.btnToggle === true) {
-            this.statusChange = 'ACTIVE';
-        } else {
-            this.statusChange = 'INACTIVE';
-        }
+        this.breadItems = [
+            { label: this.globals.languageJson?.home, routerLink: '/features/welcome-aboard' },
+            { label: 'Farm Link' },
+            { label: this.globals.languageJson?.brand_profile },
+        ];
     }
 }
