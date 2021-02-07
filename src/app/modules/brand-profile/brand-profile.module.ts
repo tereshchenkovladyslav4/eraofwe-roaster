f7040daa4df6a90e535cf25d3fbc1a5084604469
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-02-07 22:59:07 +0300

    make about us routing

--- src/app/modules/brand-profile/brand-profile.module.ts
+++ src/app/modules/brand-profile/brand-profile.module.ts
@@ -6,13 +6,22 @@ import { SharedModule } from '@shared';
 
 import { BrandProfileComponent } from './brand-profile.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 import { LearnComponent } from './learn/learn.component';
 import { SustainabilityComponent } from './sustainability/sustainability.component';
 import { VisitUsComponent } from './visit-us/visit-us.component';
 import { FeaturedProductsComponent } from './featured-products/featured-products.component';
 
 @NgModule({
-    declarations: [BrandProfileComponent, HomePageComponent, LearnComponent, SustainabilityComponent, VisitUsComponent, FeaturedProductsComponent],
+    declarations: [
+        BrandProfileComponent,
+        HomePageComponent,
+        AboutUsComponent,
+        LearnComponent,
+        SustainabilityComponent,
+        VisitUsComponent,
+        FeaturedProductsComponent,
+    ],
     imports: [CommonModule, BrandProfileRoutingModule, SharedModule],
 })
 export class BrandProfileModule {}
