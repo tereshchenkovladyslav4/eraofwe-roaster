ff06d2f70f9c4e729a83457d1ababd62123b6fd1
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-02-20 10:16:20 +0530

    Vat management changes

--- src/app/features/vat-management/vat-management.component.ts
+++ src/app/features/vat-management/vat-management.component.ts
@@ -7,186 +7,180 @@ import { AnyARecord } from 'dns';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-vat-management',
-  templateUrl: './vat-management.component.html',
-  styleUrls: ['./vat-management.component.scss']
+    selector: 'app-vat-management',
+    templateUrl: './vat-management.component.html',
+    styleUrls: ['./vat-management.component.scss'],
 })
 export class VatManagementComponent implements OnInit {
-	// tslint:disable: indent
-	saveshippingmode = false;
-	editshippingmode = true;
-	roasterId: any;
-	shippingName: any;
-	dayMin: any;
-	dayMax: any;
-	shippingPrice: any;
-	shipId: any;
-	shippData: any;
-	resetButtonValue = 'Save';
-	breadItems: any[];
-	options: any;
-	selectedTab = {name: 'Vat Management', code: '', index: 0};
-	selectedIndex = 0;
-	details: FormGroup;
-	shippingDetails = {
-		id: '',
-		name: '',
-		price: '',
-		day_min: '',
-		day_max: ''
-	};
-	selectedMobileTab = '';
-	constructor(
-		private router: Router,
-		private toastrService: ToastrService,
-		public userService: UserserviceService,
-		public cookieService: CookieService,
-		private route: ActivatedRoute,
-		public fb: FormBuilder
-		) {
-		this.roasterId = this.cookieService.get('roaster_id');
-	}
+    // tslint:disable: indent
+    saveshippingmode = false;
+    editshippingmode = true;
+    roasterId: any;
+    shippingName: any;
+    dayMin: any;
+    dayMax: any;
+    shippingPrice: any;
+    shipId: any;
+    shippData: any;
+    resetButtonValue = 'Save';
+    breadItems: any[];
+    options: any;
+    selectedTab = { name: 'Vat Management', code: '', index: 0 };
+    selectedIndex = 0;
+    details: FormGroup;
+    shippingDetails = {
+        id: '',
+        name: '',
+        price: '',
+        day_min: '',
+        day_max: '',
+    };
+    selectedMobileTab = '';
+    constructor(
+        private router: Router,
+        private toastrService: ToastrService,
+        public userService: UserserviceService,
+        public cookieService: CookieService,
+        private route: ActivatedRoute,
+        public fb: FormBuilder,
+    ) {
+        this.roasterId = this.cookieService.get('roaster_id');
+    }
 
-	ngOnInit(): void {
-		this.getShippingInfo();
-		this.options = [
-			{name: 'Vat Management', code: '', index: 0},
-			{name: 'Shipping Details', code: '', index: 1}
-		];
-		this.route.queryParams.subscribe( params => {
-			if (params.type){
+    ngOnInit(): void {
+        this.getShippingInfo();
+        this.options = [
+            { name: 'Vat Management', code: '', index: 0 },
+            { name: 'Shipping Details', code: '', index: 1 },
+        ];
+        this.route.queryParams.subscribe((params) => {
+            if (params.type) {
 				this.selectedMobileTab = params.type;
+				this.selectedTab = params.type === 'VAT' ? this.options[0] : this.options[1];
 				this.settingBreadCrumb(params.type);
-			} else {
-				this.selectedMobileTab = '';
-				// this.selectTabs();
-				this.settingBreadCrumb();
-			}
-		});
-	}
-	settingBreadCrumb(type = null){
-		if (type){
-			this.breadItems = [
-				{ label: 'Home', routerLink: '/features/welcome-aboard' },
-				{ label: 'Product Settings', routerLink: '/features/vat-management' },
-				{ label: type === 'VAT' ? 'VAT Management' : 'Shipping Details'}
-			];
-		} else {
-			this.breadItems = [
-				{ label: 'Home', routerLink: '/features/welcome-aboard' },
-				{ label: 'Product Settings' },
-			];
-		}
-	}
-	getShippingInfo(){
-		this.userService.getRoasterShippingTypes(this.roasterId).subscribe(
-			result => {
-				if (result.success){
-					this.shippData = result.result;
-					if (this.shippData.length !== 0){
-						this.shippingDetails = this.shippData[0];
-						this.shipId = this.shippingDetails.id;
-						this.details = this.fb.group({
-							name: [this.shippingDetails.name, Validators.compose([Validators.required])],
-							day_min: [this.shippingDetails.day_min, Validators.compose([Validators.required])],
-							day_max: [this.shippingDetails.day_max, Validators.compose([Validators.required])],
-							price: [this.shippingDetails.price, Validators.compose([Validators.required])],
-						});
-					} else {
-						this.details = this.fb.group({
-							name: ['', Validators.compose([Validators.required])],
-							day_min: [null, Validators.compose([Validators.required])],
-							day_max: [null, Validators.compose([Validators.required])],
-							price: [null, Validators.compose([Validators.required])],
-						});
-					}
-				}
-			}
-		);
-	}
-	private validateInput(){
-		let flag = true;
-		if (this.shippingName === '' || this.dayMin === '' || this.dayMax === '' || this.shippingPrice === ''){
-			flag = false;
-		}
-		return flag;
-	}
+            } else {
+                this.selectedMobileTab = '';
+                this.settingBreadCrumb();
+            }
+        });
+    }
+    settingBreadCrumb(type = null) {
+        if (type) {
+            this.breadItems = [
+                { label: 'Home', routerLink: '/features/welcome-aboard' },
+                { label: 'Product Settings', routerLink: '/features/vat-management' },
+                { label: type === 'VAT' ? 'VAT Management' : 'Shipping Details' },
+            ];
+        } else {
+            this.breadItems = [
+                { label: 'Home', routerLink: '/features/welcome-aboard' },
+                { label: 'Product Settings' },
+            ];
+        }
+    }
+    getShippingInfo() {
+        this.userService.getRoasterShippingTypes(this.roasterId).subscribe((result) => {
+            if (result.success) {
+                this.shippData = result.result;
+                if (this.shippData.length !== 0) {
+                    this.shippingDetails = this.shippData[0];
+                    this.shipId = this.shippingDetails.id;
+                    this.details = this.fb.group({
+                        name: [this.shippingDetails.name, Validators.compose([Validators.required])],
+                        day_min: [this.shippingDetails.day_min, Validators.compose([Validators.required])],
+                        day_max: [this.shippingDetails.day_max, Validators.compose([Validators.required])],
+                        price: [this.shippingDetails.price, Validators.compose([Validators.required])],
+                    });
+                } else {
+                    this.details = this.fb.group({
+                        name: ['', Validators.compose([Validators.required])],
+                        day_min: [null, Validators.compose([Validators.required])],
+                        day_max: [null, Validators.compose([Validators.required])],
+                        price: [null, Validators.compose([Validators.required])],
+                    });
+                }
+            }
+        });
+    }
+    private validateInput() {
+        let flag = true;
+        if (this.shippingName === '' || this.dayMin === '' || this.dayMax === '' || this.shippingPrice === '') {
+            flag = false;
+        }
+        return flag;
+    }
 
-	saveShippingInfo(){
-		if (this.shippData.length === 0){
-			this.resetButtonValue = 'Saving';
-			const body = this.details.value;
-			this.userService.addRoasterShippingDetails(this.roasterId, body).subscribe(
-			response => {
-				if (response.success){
-					this.resetButtonValue = 'Save';
-					this.toastrService.success('Shipping  Details saved successfully');
-					this.getShippingInfo();
-					this.editshippingmode = true;
-					this.saveshippingmode = false;
-				}
-				else{
-					this.toastrService.error('Error while saving shipping  details');
-					this.resetButtonValue = 'Save';
-				}
-			}
-			);
-		}
-		else{
-			if (this.shipId){
-				this.resetButtonValue = 'Saving';
-				const data = this.details.value;
-				this.userService.updateRoasterShippingTypes(this.roasterId, this.shipId, data).subscribe(
-				res => {
-					if (res.success){
-						this.resetButtonValue = 'Save';
-						this.toastrService.success('Shipping Details Updated successfully');
-						this.getShippingInfo();
-						this.editshippingmode = true;
-						this.saveshippingmode = false;
-					}
-					else{
-						this.toastrService.error('Error while updating shipping  details');
-						this.resetButtonValue = 'Save';
-					}
-				}
-				);
-			}
-		}
-	}
+    saveShippingInfo() {
+        if (this.shippData.length === 0) {
+            this.resetButtonValue = 'Saving';
+            const body = this.details.value;
+            this.userService.addRoasterShippingDetails(this.roasterId, body).subscribe((response) => {
+                if (response.success) {
+                    this.resetButtonValue = 'Save';
+                    this.toastrService.success('Shipping  Details saved successfully');
+                    this.getShippingInfo();
+                    this.editshippingmode = true;
+                    this.saveshippingmode = false;
+                } else {
+                    this.toastrService.error('Error while saving shipping  details');
+                    this.resetButtonValue = 'Save';
+                }
+            });
+        } else {
+            if (this.shipId) {
+                this.resetButtonValue = 'Saving';
+                const data = this.details.value;
+                this.userService.updateRoasterShippingTypes(this.roasterId, this.shipId, data).subscribe((res) => {
+                    if (res.success) {
+                        this.resetButtonValue = 'Save';
+                        this.toastrService.success('Shipping Details Updated successfully');
+                        this.getShippingInfo();
+                        this.editshippingmode = true;
+                        this.saveshippingmode = false;
+                    } else {
+                        this.toastrService.error('Error while updating shipping  details');
+                        this.resetButtonValue = 'Save';
+                    }
+                });
+            }
+        }
+    }
 
-	editShippingInfo(){
-		this.saveshippingmode = true;
-		this.editshippingmode = false;
-	}
-	get detailsFormControl() {
-		return this.details.controls;
-	}
-	selectTabs(){
-		if (this.selectedTab.index === 0){
-			this.selectMobileTab('VAT');
-		} else {
-			this.selectMobileTab('Shipment');
-		}
-	}
+    editShippingInfo() {
+        this.saveshippingmode = true;
+        this.editshippingmode = false;
+    }
+    get detailsFormControl() {
+        return this.details.controls;
+    }
+    selectTabs() {
+        if (this.selectedTab.index === 0) {
+            this.selectMobileTab('VAT');
+        } else {
+            this.selectMobileTab('Shipment');
+        }
+    }
 
-	// Mobile view methods
-	selectMobileTab(feature){
-		this.selectedMobileTab = feature;
-		this.router.navigate(['/features/vat-management'], {queryParams: {type: feature}});
-	}
-	getHeading(){
-		let header = 'Product Settings';
-		if (this.selectedMobileTab){
-			header = this.selectedMobileTab === 'VAT' ? 'VAT Management' : 'Shipment Details';
-		}
-		return header;
-	}
-	getDescription(){
-		let description = 'Set-up your billing details, terms and conditions of use, privacy and cookie policies here.';
-		if (this.selectedMobileTab){
-			description = this.selectedMobileTab === 'VAT' ? 'Manage VAT for green coffee and B2B orders' : 'Set shipping price and estimated delivery time';
-		}
-		return description;
-	}
+    // Mobile view methods
+    selectMobileTab(feature) {
+        this.selectedMobileTab = feature;
+        this.router.navigate(['/features/vat-management'], { queryParams: { type: feature } });
+    }
+    getHeading() {
+        let header = 'Product Settings';
+        if (this.selectedMobileTab) {
+            header = this.selectedMobileTab === 'VAT' ? 'VAT Management' : 'Shipment Details';
+        }
+        return header;
+    }
+    getDescription() {
+        let description = 'Set-up your billing details, terms and conditions of use, privacy and cookie policies here.';
+        if (this.selectedMobileTab) {
+            description =
+                this.selectedMobileTab === 'VAT'
+                    ? 'Manage VAT for green coffee and B2B orders'
+                    : 'Set shipping price and estimated delivery time';
+        }
+        return description;
+    }
 }
--- src/app/features/vat-management/vat-micro-roaster/vat-micro-roaster.component.ts
+++ src/app/features/vat-management/vat-micro-roaster/vat-micro-roaster.component.ts
@@ -7,234 +7,221 @@ import { CookieService } from 'ngx-cookie-service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-vat-micro-roaster',
-  templateUrl: './vat-micro-roaster.component.html',
-  styleUrls: ['./vat-micro-roaster.component.scss']
+    selector: 'app-vat-micro-roaster',
+    templateUrl: './vat-micro-roaster.component.html',
+    styleUrls: ['./vat-micro-roaster.component.scss'],
 })
 export class VatMicroRoasterComponent implements OnInit {
-	// tslint:disable: indent
-	// tslint:disable: variable-name
-	country: any = '';
-	transaction_type: any;
-	vat_percentage: any;
-	mraddtranscation = false;
-	mradd = true;
-	editTableRow = false;
-	updateVatmode = false;
-	addMr = [];
+    // tslint:disable: indent
+    // tslint:disable: variable-name
+    country: any = '';
+    transaction_type: any;
+    vat_percentage: any;
+    mraddtranscation = false;
+    mradd = true;
+    editTableRow = false;
+    updateVatmode = false;
+    addMr = [];
 
-	roasterId: any;
-	showpostdiv = true;
-	resetButtonValue = 'Save';
-	eachId: any;
-	mrList: any;
-	editmode = true;
-	savemode = false;
-	editIndex = null;
-	transaction: FormGroup;
+    roasterId: any;
+    showpostdiv = true;
+    resetButtonValue = 'Save';
+    eachId: any;
+    mrList: any;
+    editmode = true;
+    savemode = false;
+    editIndex = null;
+    transaction: FormGroup;
 
-	@Input() mobile = false;
+    @Input() mobile = false;
 
-  constructor(
-	private router: Router,
-	private toastrService: ToastrService,
-	public cookieService: CookieService,
-	public roasteryProfileService: RoasteryProfileService,
-	public userService: UserserviceService,
-	private fb: FormBuilder
+    constructor(
+        private router: Router,
+        private toastrService: ToastrService,
+        public cookieService: CookieService,
+        public roasteryProfileService: RoasteryProfileService,
+        public userService: UserserviceService,
+        private fb: FormBuilder,
     ) {
-		this.roasterId = this.cookieService.get('roaster_id');
-		this.getVatDetails();
-	}
+        this.roasterId = this.cookieService.get('roaster_id');
+        this.getVatDetails();
+    }
 
-	ngOnInit(): void {
-		this.transaction = this.fb.group({
-			country: ['', Validators.compose([Validators.required])],
-			transaction_type: [null, Validators.compose([Validators.required])],
-			vat_percentage: [null, Validators.compose([Validators.required])],
-			vat_type: ['mr']
-		});
-	}
-	getVatDetails(){
-		this.userService.getRoasterVatDetails(this.roasterId, 'mr').subscribe(
-			data => {
-				this.mrList = data.result;
-			}
-		);
-	}
-	public addNewTranscation(){
+    ngOnInit(): void {
+        this.transaction = this.fb.group({
+            country: ['', Validators.compose([Validators.required])],
+            transaction_type: [null, Validators.compose([Validators.required])],
+            vat_percentage: [null, Validators.compose([Validators.required])],
+            vat_type: ['mr'],
+        });
+    }
+    getVatDetails() {
+        this.userService.getRoasterVatDetails(this.roasterId, 'mr').subscribe((data) => {
+            this.mrList = data.result;
+        });
+    }
+    public addNewTranscation() {
+        if (this.mobile) {
+            if (!this.editIndex) {
+                this.editIndex = this.mrList.length;
+                this.mrList.push({
+                    country: '',
+                    transaction_type: '',
+                    vat_percentage: '',
+                });
+                this.transaction = this.fb.group({
+                    country: ['', Validators.compose([Validators.required])],
+                    transaction_type: [null, Validators.compose([Validators.required])],
+                    vat_percentage: [null, Validators.compose([Validators.required])],
+                    vat_type: ['mr'],
+                });
+            } else {
+                this.toastrService.error('Please save the unsaved changes.');
+            }
+        } else {
+            this.addMr.push({
+                country: '',
+                transaction_type: '',
+                vat_percentage: '',
+            });
+            this.editmode = true;
+            this.savemode = true;
+        }
+    }
+    private validateInput(data) {
+        let flag = true;
+        if (data && data.length) {
+            data.forEach((ele) => {
+                if (ele.country === '' || ele.transaction_type === '' || ele.vat_percentage === '') {
+                    flag = false;
+                }
+            });
+        }
+        return flag;
+    }
+    public deleteRow(index) {
+        if (this.mobile) {
+            this.mrList.splice(index, 1);
+            this.editIndex = null;
+        } else {
+            this.addMr.splice(index, 1);
+        }
+    }
+    cancelItem(item, index) {
+        if (item.id) {
+            this.editIndex = null;
+        } else {
+            this.deleteRow(index);
+        }
+    }
 
-		if (this.mobile){
-			if (!this.editIndex){
-				this.editIndex = this.mrList.length;
-				this.mrList.push({
-					country: '',
-					transaction_type: '',
-					vat_percentage: '',
-				});
-				this.transaction = this.fb.group({
-					country: ['', Validators.compose([Validators.required])],
-					transaction_type: [null, Validators.compose([Validators.required])],
-					vat_percentage: [null, Validators.compose([Validators.required])],
-					vat_type: ['mr']
-				});
-			} else {
-				this.toastrService.error('Please save the unsaved changes.');
-			}
-		} else {
-			this.addMr.push({
-			country : '',
-			transaction_type : '',
-			vat_percentage: ''
-			});
-			this.editmode = true;
-			this.savemode = true;
-		}
-	}
-	private validateInput(data){
-		let flag = true;
-		if (data && data.length){
-		  data.forEach( ele => {
-			if (ele.country === '' || ele.transaction_type === '' || ele.vat_percentage === '' ){
-			  flag = false;
-			}
-		  });
-		}
-		return flag;
-	}
-	public deleteRow(index){
-		if (this.mobile){
-			this.mrList.splice(index, 1);
-			this.editIndex = null;
-		} else {
-			this.addMr.splice(index, 1);
-		}
-	}
-	cancelItem(item, index){
-		if (item.id){
-			this.editIndex = null;
-		} else {
-			this.deleteRow(index);
-		}
-	}
+    getCountryName(code: any) {
+        const country = this.roasteryProfileService.countryList.find((con) => con.isoCode === code);
+        return country ? country.name : '';
+    }
+    changeCountry() {
+        this.roasteryProfileService.changeCountry(this.roasteryProfileService.country);
+    }
+    saveMrVat() {
+        let flag = true;
+        const input = this.addMr;
+        flag = this.validateInput(input);
+        if (flag) {
+            this.resetButtonValue = 'Saving';
+            this.addMr.forEach((element) => {
+                const body = {
+                    country: element.country,
+                    transaction_type: element.transaction_type,
+                    vat_percentage: element.vat_percentage,
+                    vat_type: 'mr',
+                };
+                this.addNewVatItem(body);
+            });
+        } else {
+            this.resetButtonValue = 'Save';
+            this.toastrService.error('Fields should not be empty.');
+        }
+    }
+    addNewVatItem(body) {
+        this.userService.addVatDetails(this.roasterId, body).subscribe((result) => {
+            if (result.success) {
+                this.resetButtonValue = 'Save';
+                this.toastrService.success('Micro roaster VAT Details added successfully');
+                this.getVatDetails();
+                this.editIndex = null;
+                this.addMr = [];
+                this.editmode = true;
+                this.savemode = false;
+            } else {
+                this.toastrService.error('Error while adding VAT details');
+                this.resetButtonValue = 'Save';
+            }
+        });
+    }
+    updateEachVat(vatItem: any, index = null) {
+        this.eachId = vatItem.id;
+        if (this.mobile) {
+            if (this.editIndex === null) {
+                this.editIndex = index;
+                this.transaction = this.fb.group({
+                    country: [vatItem.country.toUpperCase(), Validators.compose([Validators.required])],
+                    transaction_type: [vatItem.transaction_type, Validators.compose([Validators.required])],
+                    vat_percentage: [vatItem.vat_percentage, Validators.compose([Validators.required])],
+                    vat_type: ['mr'],
+                });
+            } else {
+                this.toastrService.error('Please save the unsaved changes.');
+            }
+        } else {
+            this.updateVatmode = false;
+            this.editTableRow = true;
+            vatItem.country = vatItem.country.toUpperCase();
+        }
+    }
 
-	getCountryName(code: any){
-		const country = this.roasteryProfileService.countryList.find(con => con.isoCode === code);
-		return country ? country.name : '';
-	}
-	changeCountry() {
-		this.roasteryProfileService.changeCountry(this.roasteryProfileService.country);
-	}
-	saveMrVat(){
-		let flag = true;
-		const input = this.addMr;
-		flag = this.validateInput(input);
-		if (flag){
-			this.resetButtonValue = 'Saving';
-			this.addMr.forEach(element => {
-				const body = {
-					country : element.country,
-					transaction_type : element.transaction_type,
-					vat_percentage : element.vat_percentage,
-					vat_type: 'mr'
-				};
-				this.addNewVatItem(body);
-		    });
-		}
-		else {
-			this.resetButtonValue = 'Save';
-			this.toastrService.error('Fields should not be empty.');
-		  }
-	}
-	addNewVatItem(body){
-		this.userService.addVatDetails(this.roasterId, body).subscribe(
-			result => {
-				if (result.success){
-					this.resetButtonValue = 'Save';
-					this.toastrService.success('Micro roaster VAT Details added successfully');
-					this.getVatDetails();
-					this.editIndex = null;
-					this.addMr = [];
-					this.editmode = true;
-					this.savemode = false;
-				}
-				else{
-					this.toastrService.error('Error while adding VAT details');
-					this. resetButtonValue = 'Save';
-				}
-			}
-			);
-	}
-	updateEachVat(vatItem: any, index = null){
-		this.eachId = vatItem.id;
-		if (this.mobile){
-			if (this.editIndex === null){
-				this.editIndex = index;
-				this.transaction = this.fb.group({
-					country: [vatItem.country.toUpperCase(), Validators.compose([Validators.required])],
-					transaction_type: [vatItem.transaction_type, Validators.compose([Validators.required])],
-					vat_percentage: [vatItem.vat_percentage, Validators.compose([Validators.required])],
-					vat_type: ['mr']
-				});
-			} else {
-				this.toastrService.error('Please save the unsaved changes.');
-			}
-		} else {
-			this.updateVatmode = false;
-			this.editTableRow = true;
-			vatItem.country = vatItem.country.toUpperCase();
-		}
-	}
-
-	saveEachMrVat(vatId: any, data = null, tab = false){
-		let updateData = {
-			country : data.country,
-			transaction_type : data.transaction_type,
-			vat_percentage : data.vat_percentage,
-			vat_type: 'mr'
-		};
-		if (this.mobile){
-			if (vatId){
-				updateData = tab ? updateData : data;
-			} else {
-				updateData = tab ? updateData : data;
-				this.addNewVatItem(updateData);
-				return;
-			}
-		}
-		this.userService.updateMrVat(this.roasterId, updateData, vatId).subscribe(
-			result => {
-				if (result.success){
-					this.toastrService.success('Micro roaster VAT Details updated successfully');
-					this.getVatDetails();
-					this.editIndex = null;
-					this.updateVatmode = true;
-					this.editTableRow = false;
-				}
-				else{
-					this.toastrService.error('Error while adding VAT details');
-				}
-			}
-		);
-	}
-	deleteEachVat(deleteId: any){
-		this.userService.deleteMrVat(this.roasterId, deleteId).subscribe(
-			result => {
-				if (result.success){
-					this.toastrService.success('Micro roaster VAT deleted successfully');
-					setTimeout(() => {
-						this.getVatDetails();
-					}, 1000);
-					this.editIndex = null;
-					this.editmode = true;
-				}
-				else{
-					this.toastrService.error('Error while deleting VAT details');
-				}
-			}
-		);
-	}
-	get detailsFormControl() {
-		return this.transaction.controls;
-	}
+    saveEachMrVat(vatId: any, data = null, tab = false) {
+        let updateData = {
+            country: data.country,
+            transaction_type: data.transaction_type,
+            vat_percentage: data.vat_percentage,
+            vat_type: 'mr',
+        };
+        if (this.mobile) {
+            if (vatId) {
+                updateData = tab ? updateData : data;
+            } else {
+                updateData = tab ? updateData : data;
+                this.addNewVatItem(updateData);
+                return;
+            }
+        }
+        this.userService.updateMrVat(this.roasterId, updateData, vatId).subscribe((result) => {
+            if (result.success) {
+                this.toastrService.success('Micro roaster VAT Details updated successfully');
+                this.getVatDetails();
+                this.editIndex = null;
+                this.updateVatmode = true;
+                this.editTableRow = false;
+            } else {
+                this.toastrService.error('Error while adding VAT details');
+            }
+        });
+    }
+    deleteEachVat(deleteId: any) {
+        this.userService.deleteMrVat(this.roasterId, deleteId).subscribe((result) => {
+            if (result.success) {
+                this.toastrService.success('Micro roaster VAT deleted successfully');
+                setTimeout(() => {
+                    this.getVatDetails();
+                }, 1000);
+                this.editIndex = null;
+                this.editmode = true;
+            } else {
+                this.toastrService.error('Error while deleting VAT details');
+            }
+        });
+    }
+    get detailsFormControl() {
+        return this.transaction.controls;
+    }
 }
