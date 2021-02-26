4df4b3a9be9dba81049d63ad0a0bb33a05d221a9
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-02-26 14:45:03 +0100

    Starting UI building

--- src/app/features/api-requests/api-requests-table/api-request-details/api-request-details.component.ts
+++ src/app/features/api-requests/api-requests-table/api-request-details/api-request-details.component.ts
@@ -1,54 +1,59 @@
 import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
-
 @Component({
-  selector: 'app-api-request-details',
-  templateUrl: './api-request-details.component.html',
-  styleUrls: ['./api-request-details.component.css']
+    selector: 'app-api-request-details',
+    templateUrl: './api-request-details.component.html',
+    styleUrls: ['./api-request-details.component.css'],
 })
 export class ApiRequestDetailsComponent implements OnInit {
-  btnToggle = true;
-  statusChange: string;
-  modalRef: BsModalRef;
-  resetButtonValue: string;
+    termStatus: any;
+    btnToggle = true;
+    statusChange: string;
+    modalRef: BsModalRef;
+    resetButtonValue: string;
+    showStatus: boolean = true;
 
-  constructor(private modalService: BsModalService) {
-  
-   }
-
-   @ViewChild('pausetemplate') private pausetemplate: any;
-   @ViewChild('deletetemplate') private deletetemplate: any;
-
-   openModal(template: TemplateRef<any>) {
-     this.modalRef = this.modalService.show(template);
-   
-   }
-  ngOnInit(): void {
-    this.resetButtonValue = "Generate Key";
-  }
-  activeStatus() {
-    this.btnToggle = !this.btnToggle;
-    if(this.btnToggle == true){
-      this.statusChange = "ACTIVE";
+    constructor(private modalService: BsModalService) {
+        this.termStatus = '';
     }
-    else{
-      this.statusChange = "INACTIVE";
+
+    @ViewChild('pausetemplate') private pausetemplate: any;
+    @ViewChild('deletetemplate') private deletetemplate: any;
+
+    openModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template);
     }
-  }
+    ngOnInit(): void {
+        this.resetButtonValue = 'Generate Key';
+    }
+    activeStatus() {
+        this.btnToggle = !this.btnToggle;
+        if (this.btnToggle == true) {
+            this.statusChange = 'ACTIVE';
+        } else {
+            this.statusChange = 'INACTIVE';
+        }
+    }
 
+    setStatus(term: any) {
+        this.termStatus = term;
+        console.log(this.termStatus);
+    }
 
+    toggleStatus() {
+        this.showStatus = !this.showStatus;
+        if (this.showStatus == false) {
+            document.getElementById('status_id').style.border = '1px solid #30855c';
+        } else {
+            document.getElementById('status_id').style.border = '1px solid #d6d6d6';
+        }
+    }
 
-  delete(){
-    this.openModal(this.deletetemplate);
-  }
-  pause(){
-    this.openModal(this.pausetemplate);
-
-  }
-
-
-
- 
-
+    delete() {
+        this.openModal(this.deletetemplate);
+    }
+    pause() {
+        this.openModal(this.pausetemplate);
+    }
 }
--- src/app/features/api-requests/api-requests-table/api-requests-table.component.ts
+++ src/app/features/api-requests/api-requests-table/api-requests-table.component.ts
@@ -1,29 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { GlobalsService } from 'src/services/globals.service';
 
-
 @Component({
-  selector: 'app-api-requests-table',
-  templateUrl: './api-requests-table.component.html',
-  styleUrls: ['./api-requests-table.component.css']
+    selector: 'app-api-requests-table',
+    templateUrl: './api-requests-table.component.html',
+    styleUrls: ['./api-requests-table.component.css'],
 })
 export class ApiRequestsTableComponent implements OnInit {
-  appLanguage?: any ={};
-  greenActive:any =0;
-  constructor(public globals: GlobalsService) { }
+    showDateRange: any;
+    @ViewChild('calendar')
+    calendar: any;
+    showDisplay: boolean = true;
+    display: any;
+    rangeDates: any;
+    appLanguage?: any = {};
+    greenActive: any = 0;
+    termStatus: any;
+    showStatus: boolean = true;
+    constructor(public globals: GlobalsService) {
+        this.termStatus = '';
+        this.display = '10';
+    }
 
-  ngOnInit(): void {
-   
-    this.language();
+    ngOnInit(): void {
+        this.language();
+    }
 
-  }
+    toggleDisplay() {
+        this.showDisplay = !this.showDisplay;
+        if (this.showDisplay == false) {
+            document.getElementById('display_id').style.border = '1px solid #30855c';
+        } else {
+            document.getElementById('display_id').style.border = '1px solid #d6d6d6';
+        }
+    }
 
-  
-  language(){
-   
-    this.appLanguage = this.globals.languageJson;
-   this.greenActive++;
+    openCalendar(event: any) {
+        this.calendar.showOverlay(this.calendar.inputfieldViewChild.nativeElement);
+        event.stopPropagation();
+    }
 
-  }
+    language() {
+        this.appLanguage = this.globals.languageJson;
+        this.greenActive++;
+    }
 
+    setStatus(term: any) {
+        this.termStatus = term;
+        console.log(this.termStatus);
+    }
+
+    toggleStatus() {
+        this.showStatus = !this.showStatus;
+        if (this.showStatus == false) {
+            document.getElementById('status_id').style.border = '1px solid #30855c';
+        } else {
+            document.getElementById('status_id').style.border = '1px solid #d6d6d6';
+        }
+    }
+
+    filterDate(event: any) {}
 }
