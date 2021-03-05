84e8be4c76d37d4afdf1849c34336271b797aa14
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-03-05 13:42:46 +0530

    added changes

--- src/app/modules/green-coffee-management/green-coffee-inventory/coffee-procured-tab/coffee-procured-tab.component.ts
+++ src/app/modules/green-coffee-management/green-coffee-inventory/coffee-procured-tab/coffee-procured-tab.component.ts
@@ -226,7 +226,7 @@ export class CoffeeProcuredTabComponent implements OnInit {
     sourcingRedirect(item) {
         this.userService.getGreenCoffeeDetails(this.roasterID, item.harvest_id).subscribe((res) => {
             if (res.success) {
-                return `/sourcing/coffee-list/${res.result?.estate_id}/${res.result?.harvest_id}`;
+                return `/sourcing/coffee-details/${res.result?.estate_id}/${res.result?.harvest_id}`;
             } else {
                 this.toastrService.error('Error while getting the Availability details');
             }
