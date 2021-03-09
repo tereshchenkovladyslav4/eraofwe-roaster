06216bf503b166246861a7149de9c46cd77f45ea
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-03-09 19:22:39 +0100

    use review component

--- src/app/features/roastery-profile/reviews/reviews.component.ts
+++ src/app/features/roastery-profile/reviews/reviews.component.ts
@@ -4,9 +4,9 @@ import { GlobalsService } from '@services';
 import { RoasterserviceService } from '@services';
 
 @Component({
-    selector: 'sewn-reviews',
+    selector: 'app-sewn-reviews',
     templateUrl: './reviews.component.html',
-    styleUrls: ['./reviews.component.css'],
+    styleUrls: ['./reviews.component.scss'],
 })
 export class ReviewsComponent implements OnInit {
     totalstar = 5;
@@ -14,7 +14,6 @@ export class ReviewsComponent implements OnInit {
     reviewvalue: any = 4;
     termStatus: any;
     showRelavant = true;
-    appLanguage?: any;
     roasterId: any;
     reviews: any = [];
     isLoading?: boolean;
@@ -28,7 +27,6 @@ export class ReviewsComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.appLanguage = this.globals.languageJson;
         this.roasterId = this.cookieService.get('roaster_id');
         this.getReviews();
     }
@@ -36,6 +34,7 @@ export class ReviewsComponent implements OnInit {
     getReviews(): void {
         this.isLoading = true;
         this.roasterService.getRoasterReviews(this.roasterId).subscribe((res) => {
+            console.log('review: ', res);
             if (res.success) {
                 this.isLoading = false;
                 this.reviews = res.result ? res.result : [];
@@ -45,98 +44,6 @@ export class ReviewsComponent implements OnInit {
 
     setStatus(term: any) {
         this.termStatus = term;
-    }
-    moreMethod() {
-        const dots = document.getElementById('dots');
-        const moreText = document.getElementById('more');
-        const btnText = document.getElementById('read_id');
-
-        if (dots.style.display === 'none') {
-            dots.style.display = 'inline';
-            btnText.innerHTML = 'Read more' + '<img class="pl-1" src="/assets/images/active-review.svg" />';
-            moreText.style.display = 'none';
-        } else {
-            dots.style.display = 'none';
-            btnText.innerHTML = 'Read less' + '<img class="pl-1" src="/assets/images/user-down.svg" />';
-            moreText.style.display = 'inline';
-        }
-    }
-
-    moreLess() {
-        const dots = document.getElementById('dots_two');
-        const moreText = document.getElementById('more_two');
-        const btnText = document.getElementById('read_id_two');
-
-        if (dots.style.display === 'none') {
-            dots.style.display = 'inline';
-            btnText.innerHTML = 'Read more' + '<img class="pl-1" src="/assets/images/active-review.svg" />';
-            moreText.style.display = 'none';
-        } else {
-            dots.style.display = 'none';
-            btnText.innerHTML = 'Read less' + '<img class="pl-1" src="/assets/images/user-down.svg" />';
-            moreText.style.display = 'inline';
-        }
-    }
-    lessMethod() {
-        const dots = document.getElementById('dots_three');
-        const moreText = document.getElementById('more_three');
-        const btnText = document.getElementById('read_id_three');
-
-        if (dots.style.display === 'none') {
-            dots.style.display = 'inline';
-            btnText.innerHTML = 'Read more' + '<img class="pl-1" src="/assets/images/active-review.svg" />';
-            moreText.style.display = 'none';
-        } else {
-            dots.style.display = 'none';
-            btnText.innerHTML = 'Read less' + '<img class="pl-1" src="/assets/images/user-down.svg" />';
-            moreText.style.display = 'inline';
-        }
-    }
-
-    communicationMore_one() {
-        const dots = document.getElementById('dots_contact1');
-        const moreText = document.getElementById('more_contact1');
-        const btnText = document.getElementById('read_contact1');
-
-        if (dots.style.display === 'none') {
-            dots.style.display = 'inline';
-            btnText.innerHTML = 'Read more' + '<img class="pl-1" src="/assets/images/active-review.svg" />';
-            moreText.style.display = 'none';
-        } else {
-            dots.style.display = 'none';
-            btnText.innerHTML = 'Read less' + '<img class="pl-1" src="/assets/images/user-down.svg" />';
-            moreText.style.display = 'inline';
-        }
-    }
-    communicationMore_two() {
-        const dots = document.getElementById('dots_contact2');
-        const moreText = document.getElementById('more_contact2');
-        const btnText = document.getElementById('read_contact2');
-
-        if (dots.style.display === 'none') {
-            dots.style.display = 'inline';
-            btnText.innerHTML = 'Read more' + '<img class="pl-1" src="/assets/images/active-review.svg" />';
-            moreText.style.display = 'none';
-        } else {
-            dots.style.display = 'none';
-            btnText.innerHTML = 'Read less' + '<img class="pl-1" src="/assets/images/user-down.svg" />';
-            moreText.style.display = 'inline';
-        }
-    }
-    communicationMore_three() {
-        const dots = document.getElementById('dots_contact3');
-        const moreText = document.getElementById('more_contact3');
-        const btnText = document.getElementById('read_contact3');
-
-        if (dots.style.display === 'none') {
-            dots.style.display = 'inline';
-            btnText.innerHTML = 'Read more' + '<img class="pl-1" src="/assets/images/active-review.svg" />';
-            moreText.style.display = 'none';
-        } else {
-            dots.style.display = 'none';
-            btnText.innerHTML = 'Read less' + '<img class="pl-1" src="/assets/images/user-down.svg" />';
-            moreText.style.display = 'inline';
-        }
     }
     toggleRelavant() {
         this.showRelavant = !this.showRelavant;
