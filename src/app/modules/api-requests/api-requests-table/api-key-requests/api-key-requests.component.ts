ae54cf6308fe4314700eab982f5a6830e7370291
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-03-10 21:09:07 +0300

    remove something

--- src/app/modules/api-requests/api-requests-table/api-key-requests/api-key-requests.component.ts
+++ src/app/modules/api-requests/api-requests-table/api-key-requests/api-key-requests.component.ts
@@ -127,40 +127,4 @@ export class ApiKeyRequestsComponent implements OnInit {
         this.pageNumber = event.page + 1;
         this.getApiRequestData();
     }
-
-    setStatus(term: any) {
-        this.termStatus = term;
-        console.log(this.termStatus);
-    }
-    toggleStatus() {
-        this.showStatus = !this.showStatus;
-        if (this.showStatus === false) {
-            document.getElementById('status_id').style.border = '1px solid #30855c';
-        } else {
-            document.getElementById('status_id').style.border = '1px solid #d6d6d6';
-        }
-    }
-
-    setDisplay(displayData: any) {
-        this.display = displayData;
-    }
-    toggleDisplay() {
-        this.showDisplay = !this.showDisplay;
-        if (this.showDisplay === false) {
-            document.getElementById('display_id').style.border = '1px solid #30855c';
-        } else {
-            document.getElementById('display_id').style.border = '1px solid #d6d6d6';
-        }
-    }
-    // Function Name : CheckAll
-    // Description: This function helps to check all roles of the role list.
-    checkAll(ev: any) {
-        this.mainData.forEach((x) => (x.state = ev.target.checked));
-    }
-
-    // Function Name : IsAllchecked
-    // Description: This function helps to check single role.
-    isAllChecked() {
-        return this.mainData.every((_) => _.state);
-    }
 }
