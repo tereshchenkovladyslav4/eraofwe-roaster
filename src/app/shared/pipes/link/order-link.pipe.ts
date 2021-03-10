15cab4e186abd5282d0493aee3697d952aca4266
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-03-10 14:34:04 +0300

    move review and rating

--- src/app/shared/pipes/link/order-link.pipe.ts
+++ src/app/shared/pipes/link/order-link.pipe.ts
@@ -6,29 +6,6 @@ import { OrgType, OrderType, OrderStatus } from '@enums';
 })
 export class OrderLinkPipe implements PipeTransform {
     transform(orgType: OrgType, orderId: number, orderType: OrderType, orderStatus: OrderStatus): string {
-        switch (orgType) {
-            case OrgType.ESTATE: {
-                if (orderType === OrderType.Booked) {
-                    return `/ordermanagement/order-booked/${orderId}/${orderStatus}`;
-                } else if (orderType === OrderType.Sample) {
-                    return `/ordermanagement/order-sample/${orderId}/${orderStatus}`;
-                } else if (orderType === OrderType.Prebook) {
-                    return `/ordermanagement/order-prebook/${orderId}/${orderStatus}`;
-                }
-                break;
-            }
-            case OrgType.MICRO_ROASTER: {
-                if (orderStatus === OrderStatus.Placed) {
-                    return `/ordermanagement/booked-order-confirmation/${orderId}/${orderType}`;
-                } else if (orderStatus === OrderStatus.Rejected) {
-                    return `/ordermanagement/microroaster-orders/${orderId}/${orderType}`;
-                } else {
-                    return `/ordermanagement/mr-booked/${orderId}/${orderType}`;
-                }
-            }
-            default: {
-                return '';
-            }
-        }
+        return `/orders/${orgType}/${orderId}`;
     }
 }
