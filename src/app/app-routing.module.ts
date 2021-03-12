9558a6e5b35a019c7eb94cdac319d72f2fdbd68b
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-03-12 10:52:43 +0100

    translate, tslint issue fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
         component: LayoutComponent,
         children: [
             {
-                path: 'tes',
+                path: '',
                 loadChildren: () => import('./modules/dashboard/dashboard.module').then((m) => m.DashboardModule),
             },
             {
@@ -61,7 +61,8 @@ export const routes: Routes = [
             },
             {
                 path: 'api-requests-list',
-                loadChildren: () => import('./modules/api-requests/api-request.module').then((m) => m.ApiRequestModule),
+                loadChildren: () =>
+                    import('./modules/api-requests/api-requests.module').then((m) => m.ApiRequestModule),
             },
             {
                 path: 'green-coffee-management',
@@ -86,11 +87,6 @@ export const routes: Routes = [
                 path: 'roastery-profile',
                 loadChildren: () =>
                     import('./modules/profile-creation/profile-creation.module').then((m) => m.ProfileCreationModule),
-            },
-            {
-                path: '',
-                redirectTo: 'roastery-profile',
-                pathMatch: 'full',
             },
         ],
     },
