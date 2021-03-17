637dea6200d69c4fbd69ac7ebd02be09380e23e7
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-03-17 23:06:23 +0530

    api call done

--- src/app/modules/people/customer-management/details-discount/details-discount.component.ts
+++ src/app/modules/people/customer-management/details-discount/details-discount.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { UserserviceService } from '@services';
 import { ToastrService } from 'ngx-toastr';
 import { CustomerServiceService } from '../customer-service.service';
@@ -10,6 +10,8 @@ import { CustomerServiceService } from '../customer-service.service';
 })
 export class DetailsDiscountComponent implements OnInit, AfterViewInit {
     roasterId: any;
+    @Input() customerType: string;
+
     constructor(
         public customerService: CustomerServiceService,
         private userService: UserserviceService,
@@ -27,18 +29,31 @@ export class DetailsDiscountComponent implements OnInit, AfterViewInit {
         const discountData = {
             discount_percentage: parseFloat(this.customerService.discount_percentage),
         };
-        this.userService
-            .updateHorecaDiscount(this.roasterId, this.customerService.horecaId, discountData)
-            .subscribe((res: any) => {
-                if (res.success === true) {
-                    // this.customer.mrCustomerDetails();
-                    this.toastrService.success('Discount data updated sucessfully');
-                } else {
-                    this.toastrService.error('Error while updating discount data');
-                }
-            });
-        document.getElementById('edithoreca-discount').style.display = 'block';
-        document.getElementById('savehoreca-discount').style.display = 'none';
+        if (this.customerType === 'hrc') {
+            this.userService
+                .updateHorecaDiscount(this.roasterId, this.customerService.horecaId, discountData)
+                .subscribe((res: any) => {
+                    if (res.success) {
+                        this.toastrService.success('Discount data updated sucessfully');
+                    } else {
+                        this.toastrService.error('Error while updating discount data');
+                    }
+                });
+            document.getElementById('edithoreca-discount').style.display = 'block';
+            document.getElementById('savehoreca-discount').style.display = 'none';
+        } else if (this.customerType === 'micro-roasters') {
+            this.userService
+                .updateMicroDiscount(this.roasterId, this.customerService.microId, discountData)
+                .subscribe((res: any) => {
+                    if (res.success) {
+                        this.toastrService.success('Discount data updated sucessfully');
+                    } else {
+                        this.toastrService.error('Error while updating discount data');
+                    }
+                });
+            document.getElementById('edit-discount').style.display = 'block';
+            document.getElementById('save-discount').style.display = 'none';
+        }
     }
 
     ngAfterViewInit() {
