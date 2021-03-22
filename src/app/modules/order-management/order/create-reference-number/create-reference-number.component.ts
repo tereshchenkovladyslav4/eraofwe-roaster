6428d5b0c48a235277820cbb6062f9f975db6b78
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-03-22 10:58:29 +0300

    #1942 - Order reference number editor

--- src/app/modules/order-management/order/create-reference-number/create-reference-number.component.ts
+++ src/app/modules/order-management/order/create-reference-number/create-reference-number.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { OrderManagementService } from '../../order-management.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
     selector: 'app-create-reference-number',
@@ -10,7 +11,7 @@ export class CreateReferenceNumberComponent {
     @Input() refNumber: string;
     @Input() orderId: number;
 
-    constructor(private orderService: OrderManagementService) {}
+    constructor(private toasterSrv: ToastrService, private orderService: OrderManagementService) {}
 
     save(val: string): void {
         const trimmedVal = (val || '').trim();
@@ -18,6 +19,7 @@ export class CreateReferenceNumberComponent {
             this.orderService.createReferenceNumber(this.orderId, trimmedVal).subscribe((res) => {
                 if (res.success) {
                     this.refNumber = trimmedVal;
+                    this.toasterSrv.success('Order reference number has been updated.');
                 }
             });
         }
