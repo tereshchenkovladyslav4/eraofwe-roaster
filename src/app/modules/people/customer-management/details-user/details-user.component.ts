c06c05b7b03661fb0258641ff5d4ecf492a8a67a
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-03-24 16:10:10 +0530

    Message integrated

--- src/app/modules/people/customer-management/details-user/details-user.component.ts
+++ src/app/modules/people/customer-management/details-user/details-user.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerServiceService } from '../customer-service.service';
+import { ChatHandlerService } from '@services';
+import { OrganizationType } from '@enums';
+import { SourcingService } from '@app/modules/sourcing/sourcing.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
     selector: 'app-details-user',
@@ -7,7 +11,20 @@ import { CustomerServiceService } from '../customer-service.service';
     styleUrls: ['./details-user.component.scss'],
 })
 export class DetailsUserComponent implements OnInit {
-    constructor(public customerService: CustomerServiceService) {}
+    constructor(
+        public customerService: CustomerServiceService,
+        public sourcing: SourcingService,
+        private chatHandler: ChatHandlerService,
+        private cookieService: CookieService,
+    ) {}
 
     ngOnInit(): void {}
+
+    openChat(): void {
+        this.chatHandler.openChatThread({
+            user_id: parseInt(this.customerService.admin_id, 10),
+            org_type: OrganizationType.ESTATE,
+            org_id: parseInt(this.cookieService.get('roaster_id'), 10),
+        });
+    }
 }
