e2ee5d7cec5c0636ac6368988fdbe7196ec80c7d
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-04-05 13:57:04 +0300

    fix health check

--- src/app/components/health-check/health-check.component.ts
+++ src/app/components/health-check/health-check.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-health-check',
-  templateUrl: './health-check.component.html',
-  styleUrls: ['./health-check.component.css']
+    selector: 'app-health-check',
+    templateUrl: './health-check.component.html',
 })
 export class HealthCheckComponent implements OnInit {
-  object: Object = {status: '200 ok', message: 'ok'};
-  constructor() { }
+    readonly object: object = { status: '200 ok', message: 'ok' };
+    constructor() {}
 
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
