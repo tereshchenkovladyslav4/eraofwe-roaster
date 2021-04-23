70eba9cd9538861cca579d17d05f32091ec475b4
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-04-23 09:32:39 +0200

    clean code in mr profile creation

--- src/app/modules/profiles/micro-profile/micro-profile.service.ts
+++ src/app/modules/profiles/micro-profile/micro-profile.service.ts
@@ -4,7 +4,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { RoasterserviceService } from '@services';
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
-import { COUNTRY_LIST, organizationTypes } from '@constants';
+import { COUNTRY_LIST } from '@constants';
 import { BehaviorSubject } from 'rxjs';
 import { MicroOrganizationProfile } from '@models';
 import { OrganizationType } from '@enums';
@@ -13,36 +13,12 @@ import { OrganizationType } from '@enums';
     providedIn: 'root',
 })
 export class MicroProfileService {
-    countryList = [];
-
-    public editMode = new BehaviorSubject(true);
-    public editMode$ = this.editMode.asObservable();
-
-    public saveMode = new BehaviorSubject(false);
-    public saveMode$ = this.saveMode.asObservable();
-
-    public avatarImageChanged = new BehaviorSubject(null);
-    public avatarImageChanged$ = this.avatarImageChanged.asObservable();
-
-    public mainSubFormInvalid = false;
-    public aboutFormInvalid = false;
-    public contactFormInvalid = false;
-    public toUpdateProfileData: MicroOrganizationProfile;
     public roasteryProfileData: MicroOrganizationProfile;
-
-    public invalidSumEmployee = false;
-
-    cities: Array<any> = [];
 
     userId: string;
     roasterUsers: any = [];
     roasterContacts: any = [];
     single: { name: string; value: any }[];
-    showDelete = false;
-    bannerUrl?: string;
-    bannerFile?: any;
-    profileInfo?: any;
-    isSaving?: boolean;
     roasterId: string;
 
     constructor(
@@ -54,7 +30,6 @@ export class MicroProfileService {
     ) {
         this.userId = this.cookieService.get('user_id');
         this.roasterId = this.cookieService.get('roaster_id');
-        this.countryList = COUNTRY_LIST;
     }
 
     roasterProfile(microRoasterId) {
@@ -62,7 +37,6 @@ export class MicroProfileService {
             console.log('micro roaster details: ', result);
             if (result.success) {
                 this.roasteryProfileData = result.result;
-                this.toUpdateProfileData = result.result;
                 this.single = [
                     {
                         name: 'Female',
@@ -77,16 +51,12 @@ export class MicroProfileService {
                             : 0,
                     },
                 ];
-                this.saveMode.next(false);
-                this.editMode.next(true);
             }
         });
 
         this.roasterService.getRoasterUsers(microRoasterId).subscribe((data: any) => {
             if (data.success) {
                 this.roasterUsers = data.result;
-
-                console.log(this.roasterUsers);
             }
         });
 
@@ -100,9 +70,5 @@ export class MicroProfileService {
                 console.log(this.roasterContacts);
             }
         });
-    }
-
-    changeCountry(count) {
-        this.cities = COUNTRY_LIST.find((con) => con.isoCode === count).cities;
     }
 }
