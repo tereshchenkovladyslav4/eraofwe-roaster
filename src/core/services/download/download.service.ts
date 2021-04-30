913f69e6aee1f00f931d8fb8dc24a28c1bf6539b
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-04-30 05:55:31 +0200

    clean code

--- src/core/services/download/download.service.ts
+++ src/core/services/download/download.service.ts
@@ -12,25 +12,6 @@ import { environment } from '@env/environment';
 export class DownloadService {
     constructor(private http: HttpClient, @Inject(SAVER) private save: Saver) {}
 
-    // download(url: string, filename?: string, contentType?: string): Observable<Download> {
-    //     const headers = {
-    //         'Content-Type': contentType,
-    //         Accept: contentType,
-    //         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-    //         'Access-Control-Allow-Origin': 'https://roaster.sewnstaging.com',
-    //         'Access-Control-Allow-Methods': 'GET, HEAD',
-    //         'Access-Control-Allow-credentials': 'true',
-    //     };
-    //     return this.http
-    //         .get(url, {
-    //             headers: new HttpHeaders(headers),
-    //             reportProgress: true,
-    //             observe: 'events',
-    //             responseType: 'blob',
-    //         })
-    //         .pipe(download((blob) => this.save(blob, filename)));
-    // }
-
     blob(url: string, filename?: string, contentType?: string): Observable<Blob> {
         return this.http.get(url, {
             responseType: 'blob',
@@ -38,14 +19,16 @@ export class DownloadService {
     }
 
     download(url: string, filename?: string, contentType?: string): Observable<any> {
-        return this.http.post(
-            `${environment.apiURL}/images/generate-blob`,
-            { url },
-            {
-                responseType: 'blob',
-                reportProgress: true,
-                observe: 'events',
-            })
+        return this.http
+            .post(
+                `${environment.apiURL}/images/generate-blob`,
+                { url },
+                {
+                    responseType: 'blob',
+                    reportProgress: true,
+                    observe: 'events',
+                },
+            )
             .pipe(download((blob) => this.save(blob, filename)));
     }
 }
