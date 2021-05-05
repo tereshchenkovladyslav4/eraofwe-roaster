511457b806b534e47010ec4bb1d2aa35ce092356
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-05-05 14:09:36 +0400

    Fixed edit article

--- src/app/modules/coffee-lab/create-post/create-article/create-article.component.ts
+++ src/app/modules/coffee-lab/create-post/create-article/create-article.component.ts
@@ -60,7 +60,7 @@ export class CreateArticleComponent implements OnInit {
                 this.coverImageId = res.result.cover_image_id;
                 this.isCoverImageUploaded = true;
                 this.content = res.result.content;
-                this.images = res.result.images;
+                this.images = res.result.images ?? [];
                 this.articleForm.patchValue({
                     title: res.result.title,
                     subtitle: res.result.subtitle,
