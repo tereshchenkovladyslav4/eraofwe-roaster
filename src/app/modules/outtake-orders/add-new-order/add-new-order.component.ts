04cd03687c50b096eedccf69fc2cb507b9a8ba3d
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-05-06 20:39:44 +0530

    Add an order created

--- src/app/modules/outtake-orders/add-new-order/add-new-order.component.ts
+++ src/app/modules/outtake-orders/add-new-order/add-new-order.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { RoasterserviceService } from '@services';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
-  selector: 'app-add-new-order',
-  templateUrl: './add-new-order.component.html',
-  styleUrls: ['./add-new-order.component.scss']
+    selector: 'app-add-new-order',
+    templateUrl: './add-new-order.component.html',
+    styleUrls: ['./add-new-order.component.scss'],
+    encapsulation: ViewEncapsulation.None,
 })
 export class AddNewOrderComponent implements OnInit {
+    roasterId: any;
+    breadItems = [
+        { label: 'Home', routerLink: '/dashboard' },
+        { label: 'Order Management', routerLink: '/outtake-orders' },
+        { label: 'Outtake Order', routerLink: '/outtake-orders' },
+        { label: 'Add a new order' },
+    ];
+    date3: any;
+    constructor(private roasterService: RoasterserviceService, private cookieService: CookieService) {
+        this.roasterId = this.cookieService.get('roaster_id');
+    }
 
-  constructor() { }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+    getOrderDetails() {
+        // this.roasterService.getViewOrderDetails(this.roasterId,);
+    }
 }
