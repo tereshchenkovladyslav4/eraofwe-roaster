4d6c711e04620d2d6c9c57a60867b0988baacd24
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-05-10 15:54:30 +0800

    new t and c page

--- src/app/modules/account-settings/privacy-settings/privacy-settings.component.ts
+++ src/app/modules/account-settings/privacy-settings/privacy-settings.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { GlobalsService, UserService, UserserviceService } from '@services';
+import { AuthService, GlobalsService } from '@services';
 import { Location } from '@angular/common';
-import { ToastrService } from 'ngx-toastr';
 import { MenuItem } from 'primeng/api';
 
 @Component({
@@ -10,43 +9,48 @@ import { MenuItem } from 'primeng/api';
     styleUrls: ['./privacy-settings.component.scss'],
 })
 export class PrivacySettingsComponent implements OnInit {
-    privacyTerms?: any = {};
-    isLoading?: boolean;
     breadcrumbItems: MenuItem[];
+    agreements: any[] = [
+        {
+            title: 'general_terms_and_conditions',
+            url: 'https://support.eraofwe.com/en/kb/articles/general-terms-conditions-user',
+            role: '',
+        },
+        {
+            title: 'general_terms_and_conditions_for_legal_entity',
+            url: 'https://support.eraofwe.com/en/kb/articles/general-terms-conditions-legal-entity',
+            role: 'Support Admin',
+        },
+        {
+            title: 'cookie_policy',
+            url: 'https://support.eraofwe.com/en/kb/articles/cookie-policy',
+            role: '',
+        },
+        {
+            title: 'privacy_policy',
+            url: 'https://support.eraofwe.com/en/kb/articles/privacy-policy',
+            role: '',
+        },
+        {
+            title: 'data_processing_agreement',
+            url: 'https://support.eraofwe.com/en/kb/articles/data-processing-agreement',
+            role: 'Support Admin',
+        },
+        // {
+        //     title: 'master_agreement_estate',
+        //     url: '',
+        //     role: ''
+        // },
+    ];
 
-    constructor(
-        private userService: UserService,
-        private userOriginalService: UserserviceService,
-        public location: Location,
-        private toastr: ToastrService,
-        public globals: GlobalsService,
-    ) {}
+    constructor(public location: Location, public globals: GlobalsService, public authService: AuthService) {}
 
     ngOnInit(): void {
-        this.getPrivacyTerms();
         this.breadcrumbItems = [
             { label: this.globals.languageJson?.home, routerLink: '/dashboard' },
             { label: this.globals.languageJson?.account_settings, routerLink: '../../account-settings' },
             { label: this.globals.languageJson?.privacy_settings },
         ];
-    }
-
-    getPrivacyTerms(): void {
-        this.isLoading = true;
-        this.userOriginalService.getPrivacyTerms().subscribe((res: any) => {
-            console.log('get privacy terms >>>>>>>>>>>>>', res);
-            this.isLoading = false;
-            if (res.success) {
-                this.privacyTerms = res.result;
-            } else {
-                this.toastr.error('Failed to get data');
-            }
-        });
-    }
-
-    onChangeData(): void {
-        this.userOriginalService.updatePrivacyTerms(this.privacyTerms).subscribe((res: any) => {
-            console.log('update data result ??????????', res);
-        });
+        console.log('current user >>>>>>>>>>>', this.authService.currentUser);
     }
 }
