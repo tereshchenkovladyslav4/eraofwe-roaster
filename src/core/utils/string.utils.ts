67329b0d4ac87a2477912d5f072667437286411e
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-05-14 20:35:00 +0300

    Updated other products

--- src/core/utils/string.utils.ts
+++ src/core/utils/string.utils.ts
@@ -2,3 +2,12 @@ export const trimCharRight = (str: string, char: string): string => {
     const regex = new RegExp(`${char}+$`, 'gi');
     return str.replace(regex, '');
 };
+
+export const trackFileName = (url: string): string => {
+    if (url) {
+        const strArr = url.split('/');
+        const lastItem = strArr[strArr.length - 1];
+        return lastItem.split('?')[0];
+    }
+    return '';
+};
