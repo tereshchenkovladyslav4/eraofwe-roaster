c413074da15ecebe71af609c64f624ad8e501601
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-07-27 00:10:44 +0530

    0831 - 0930 Working and verifying Roaster tickets RO#3510, RO#3509, RO#3508, RO#3507 (0.98) 0930 - 1030 Working and verifying Roaster tickets RO#3510, RO#3315 (1) 1426 - 1515 Inspected and fixed scrolling Table layout issue (0.82) 1515 - 1610 Inspected and fixed cell alignment on Estate and MR order listing (0.92)

--- src/app/modules/order-management/order-list/order-list.component.ts
+++ src/app/modules/order-management/order-list/order-list.component.ts
@@ -63,7 +63,7 @@ export class OrderListComponent extends ResizeableComponent implements OnInit {
     @ViewChild('requestTable') requestTable: RequestTableComponent;
 
     get customerPropertyName(): string {
-        return this.orgType === OrganizationType.ESTATE ? 'estate' : 'micro-roaster';
+        return this.orgType === OrganizationType.ESTATE ? 'Estate' : 'Micro Roaster';
     }
 
     constructor(
