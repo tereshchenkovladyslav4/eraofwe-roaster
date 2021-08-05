184f977f65e6aa4179fdcb6ffa2bcffdb5f01f70
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-08-05 21:38:09 +0300

    fix reviews component

--- src/app/shared/components/reviews/reviews.component.ts
+++ src/app/shared/components/reviews/reviews.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { OrganizationType } from '@enums';
-import { GlobalsService } from '@services';
+import { TranslateService } from '@ngx-translate/core';
+import * as _ from 'underscore';
 
 @Component({
     selector: 'app-reviews',
@@ -13,16 +14,15 @@ export class ReviewsComponent implements OnInit {
     @Input() average: any;
     @Input() orgType: OrganizationType;
     limit = 5;
-    termStatus = 'Most relevant';
+    termStatus: 'created_at' | 'total_rating' = 'created_at';
     termItems: any[];
 
-    constructor(public globals: GlobalsService) {}
+    constructor(public translateService: TranslateService) {}
 
     ngOnInit(): void {
         this.termItems = [
-            { label: this.globals.languageJson?.most_relevant, value: 'Most relevant' },
-            { label: this.globals.languageJson?.recent_reviews, value: 'Recent reviews' },
-            { label: this.globals.languageJson?.based_on_rating, value: 'Based on rating' },
+            { label: this.translateService.instant('recent_reviews'), value: 'created_at' },
+            { label: this.translateService.instant('based_on_rating'), value: 'total_rating' },
         ];
     }
 
@@ -31,6 +31,6 @@ export class ReviewsComponent implements OnInit {
     }
 
     filterCall() {
-        console.log('Term:', this.termStatus);
+        this.reviews = _.sortBy(this.reviews, this.termStatus).reverse();
     }
 }
