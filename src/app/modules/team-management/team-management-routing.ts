06fd6430123051e31f4713f3e8a23fcba989b93e
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-08-09 17:08:34 +0530

    pending issue

--- src/app/modules/team-management/team-management-routing.ts
+++ src/app/modules/team-management/team-management-routing.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
             },
             {
                 path: 'team-members',
-                component: TeamMemberTableComponent,
+                component: UserManagementComponent,
                 children: [
                     {
                         path: '',
