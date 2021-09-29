9629997c1d0f9b3116dfa43327df638c08fec183
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-09-29 18:53:26 +0300

    fix user management component

--- src/app/modules/team-management/user-management/user-management.component.ts
+++ src/app/modules/team-management/user-management/user-management.component.ts
@@ -1,15 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router, NavigationExtras, ActivatedRoute } from '@angular/router';
-import { SharedServiceService } from '@app/shared/services/shared-service.service';
-import { AuthService, CommonService, GlobalsService, RoasterserviceService, UserService } from '@services';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { ToastrService } from 'ngx-toastr';
-import { MenuItem } from 'primeng/api';
-import { ChatHandlerService } from '@services';
-import { UserStatus } from '@enums';
-import { DialogService } from 'primeng/dynamicdialog';
-import { UserManagementSearchService } from '../user-management-service';
 import { TranslateService } from '@ngx-translate/core';
+import { MenuItem } from 'primeng/api';
+import { AuthService, RoasterserviceService } from '@services';
+import { UserStatus } from '@enums';
+import { UserManagementSearchService } from '../user-management-service';
 
 @Component({
     selector: 'app-user-management',
@@ -18,35 +13,23 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class UserManagementComponent implements OnInit {
     readonly UserStatus = UserStatus;
-    roasterID: any;
     breadCrumbItem: MenuItem[] = [];
+    menuItems: MenuItem[];
     termStatus;
     termRole;
-    termRoleName;
     termSearch = '';
-    tableColumns: any = [];
     currentRoleID;
-    roleList: any = [];
+    roleList: any[] = [];
     statusFilterArray: any = [];
     isAddMember = false;
-    modalRef: BsModalRef;
-    loginId: any;
-    tableRows;
     showAddbutton = true;
-    @ViewChild('input') input: ElementRef;
-    menuItems: MenuItem[];
 
     constructor(
-        public router: Router,
         private roasterService: RoasterserviceService,
-        public globals: GlobalsService,
-        public route: ActivatedRoute,
-        public userService: UserService,
-        public sharedService: SharedServiceService,
-        public dialogSrv: DialogService,
-        private authService: AuthService,
-        private userManagementSearchService: UserManagementSearchService,
+        private route: ActivatedRoute,
+        private router: Router,
         private translator: TranslateService,
+        private userManagementSearchService: UserManagementSearchService,
     ) {}
 
     ngOnInit(): void {
@@ -62,17 +45,10 @@ export class UserManagementComponent implements OnInit {
                 command: () => (this.showAddbutton = false),
             },
         ];
-        this.sharedService.windowWidth = window.innerWidth;
-        if (this.sharedService.windowWidth <= this.sharedService.responsiveStartsAt) {
-            this.sharedService.isMobileView = true;
-        }
-        this.tableRows = 10;
         this.statusFilterArray = [
             { name: 'Active', value: 'active' },
             { name: 'Inactive', value: 'Inactive' },
         ];
-        this.loginId = this.authService.userId;
-        this.roasterID = this.authService.getOrgId();
         this.route.queryParams.subscribe((params) => {
             this.currentRoleID = Number(params.roleID);
             this.isAddMember = params.isAddMember && params.isAddMember === 'true' ? true : false;
@@ -106,7 +82,6 @@ export class UserManagementComponent implements OnInit {
                     const getCurrentRole: any = response.result.find((ele) => ele.id === this.currentRoleID);
                     if (!this.isAddMember) {
                         this.termRole = this.currentRoleID;
-                        this.termRoleName = getCurrentRole ? getCurrentRole.name : '';
                     }
                 }
                 this.roleList = response.result;
@@ -118,22 +93,11 @@ export class UserManagementComponent implements OnInit {
     }
 
     supplyBreadCrumb(): void {
-        const obj1: MenuItem = {
-            label: this.globals.languageJson?.home,
-            routerLink: '/',
-            disabled: false,
-        };
-        const obj2: MenuItem = {
-            label: this.globals.languageJson?.team_management,
-            disabled: false,
-        };
-        const obj4: MenuItem = { label: this.globals.languageJson?.manage_roles };
-        if (!this.isAddMember) {
-            obj4.label = this.globals.languageJson?.user_management;
-        }
-        this.breadCrumbItem.push(obj1);
-        this.breadCrumbItem.push(obj2);
-        this.breadCrumbItem.push(obj4);
+        this.breadCrumbItem = [
+            { label: this.translator.instant('home'), routerLink: '/' },
+            { label: this.translator.instant('team_management') },
+            { label: this.translator.instant(this.isAddMember ? 'manage_roles' : 'user_management') },
+        ];
     }
 
     inviteNewMembers() {
