357893b3543814d4c773f17e03162aafca2c0b5f
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-11-19 14:38:19 +0530

    same file upload issue resolved

--- src/app/modules/brand-profile/components/brand-uploader/brand-uploader.component.ts
+++ src/app/modules/brand-profile/components/brand-uploader/brand-uploader.component.ts
@@ -152,7 +152,6 @@ export class BrandUploaderComponent extends ResizeableComponent implements OnIni
                                         reject();
                                     },
                                 );
-                                event.target.value = '';
                             }),
                         );
                     }
