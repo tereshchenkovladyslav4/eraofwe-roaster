d99d955ddeebf47bf17326881818b50d18d392c3
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-11-19 21:26:47 +0530

    commented code removed

--- src/app/modules/micro-roaster-invite/micro-roaster-invite-details/micro-roaster-invite-details.component.ts
+++ src/app/modules/micro-roaster-invite/micro-roaster-invite-details/micro-roaster-invite-details.component.ts
@@ -27,26 +27,6 @@ export class MicroRoasterInviteDetailsComponent implements OnInit {
 
     ngOnInit(): void {
         this.getMrPublicOnboardDetails();
-        // this.data = {
-        //     name: 'Royal coffee roasters',
-        //     owner_name: 'Markel',
-        //     founded_on: 1994,
-        //     last_login: '13:48:57 | 26th March 2020',
-        //     vat_number: '12939823-329',
-        //     reg_id: '12939823-329',
-        // };
-        // this.organizationProfile = {
-        //     website: 'www.thirdwaveroasters.com',
-        //     email: 'hello@thirdwaveroasters.com',
-        //     phone: '+46 456 786 568',
-        //     country: 'sv',
-        //     address_line1: 'Hagagatan 1',
-        //     address_line2: '',
-        //     city: 'SE-113 49 Stockholm',
-        //     state: 'SWEDEN',
-        //     fb_profile: 'facebook.com/thirdwaveroasters',
-        //     ig_profile: 'instagram.com/thirdwaveroasters',
-        // };
     }
     getMrPublicOnboardDetails() {
         this.userService.getMrPublicOnboardDetails(this.roasterId, this.requestId).subscribe((res: any) => {
