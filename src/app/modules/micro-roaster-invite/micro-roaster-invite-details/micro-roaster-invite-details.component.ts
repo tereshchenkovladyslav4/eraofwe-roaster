7d60889f108150cbf74f8dc829190cd748f608e8
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2021-11-20 15:03:01 +0530

    popup added

--- src/app/modules/micro-roaster-invite/micro-roaster-invite-details/micro-roaster-invite-details.component.ts
+++ src/app/modules/micro-roaster-invite/micro-roaster-invite-details/micro-roaster-invite-details.component.ts
@@ -1,8 +1,10 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ConfirmComponent } from '@app/shared';
 import { AuthService, UserService } from '@services';
 import { ToastrService } from 'ngx-toastr';
+import { DialogService } from 'primeng/dynamicdialog';
 
 @Component({
     selector: 'app-micro-roaster-invite-details',
@@ -20,6 +22,7 @@ export class MicroRoasterInviteDetailsComponent implements OnInit {
         private authService: AuthService,
         public route: ActivatedRoute,
         private toastr: ToastrService,
+        private dialogSrv: DialogService,
     ) {
         this.roasterId = this.authService.getOrgId();
         this.requestId = this.route.snapshot.params.id;
@@ -36,17 +39,39 @@ export class MicroRoasterInviteDetailsComponent implements OnInit {
         });
     }
     onApprove() {
-        this.userService.approveMrPublicOnboard(this.roasterId, this.requestId).subscribe((res: any) => {
-            if (res.success) {
-                this.toastr.success('Approved');
-            }
-        });
+        this.dialogSrv
+            .open(ConfirmComponent, {
+                data: {
+                    title: 'Please confirm!',
+                    desp: 'Are you sure want to approve',
+                },
+            })
+            .onClose.subscribe((action: any) => {
+                if (action === 'yes') {
+                    this.userService.approveMrPublicOnboard(this.roasterId, this.requestId).subscribe((res: any) => {
+                        if (res.success) {
+                            this.toastr.success('Approved');
+                        }
+                    });
+                }
+            });
     }
     onReject() {
-        this.userService.rejectMrPublicOnboard(this.roasterId, this.requestId).subscribe((res: any) => {
-            if (res.success) {
-                this.toastr.error('Rejected');
-            }
-        });
+        this.dialogSrv
+            .open(ConfirmComponent, {
+                data: {
+                    title: 'Please confirm!',
+                    desp: 'Are you sure want to reject',
+                },
+            })
+            .onClose.subscribe((action: any) => {
+                if (action === 'yes') {
+                    this.userService.rejectMrPublicOnboard(this.roasterId, this.requestId).subscribe((res: any) => {
+                        if (res.success) {
+                            this.toastr.error('Rejected');
+                        }
+                    });
+                }
+            });
     }
 }
