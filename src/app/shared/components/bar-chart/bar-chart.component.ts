1a412cba1ec7525a0d6f1b6cb531d2bce701336e
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2022-02-08 23:50:38 +0300

    Remove font weight bold

--- src/app/shared/components/bar-chart/bar-chart.component.ts
+++ src/app/shared/components/bar-chart/bar-chart.component.ts
@@ -9,7 +9,7 @@ import {
     ViewChild,
 } from '@angular/core';
 import { ThousandSuffPipe } from '@app/shared/pipes/thousand-suff.pipe';
-import { GENERAL_FONT_FAMILY } from '@constants';
+import { FW_B, GENERAL_FONT_FAMILY } from '@constants';
 
 import * as echarts from 'echarts';
 import ECharts = echarts.ECharts;
@@ -48,7 +48,7 @@ export class BarChartComponent implements OnInit, OnChanges {
                 const icon0 = `<span data-tooltip="minimum" style="border-left: 2px solid #fff;display: inline-block;height: 12px;margin-right: 5px;width: 20px;"><span style="background-color:${params[0].color};display: block;height: 4px;margin-top: 4px;width: 20px;"></span></span>`;
                 const icon1 = `<span data-tooltip="implied-high" style="background-color:rgba(255,255,255,.75);border-radius: 2px;display: inline-block;height: 12px;margin-right:5px;width: 20px;"><span style="background-color:${params[0].color};border: 1px solid ${params[0].color};border-radius:50%;display:block;height:6px;margin-left:7px;margin-top:3px;width:6px;"></span></span>`;
                 const convertedValue = this.thousandSuffPipe.transform(params[0].value, 0);
-                return `<span style="font-weight: bold;">${params[0].name}</span><br/> ${convertedValue}`;
+                return `<span style="font-weight: ${FW_B};">${params[0].name}</span><br/> ${convertedValue}`;
             },
         },
         xAxis: {
