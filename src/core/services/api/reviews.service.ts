0e7d279aa9d26ca643af2269b015a13ac17b1f37
Vladyslav Tereshchenko <tereshchenkovladyslav4@outlook.com>
2022-03-04 14:15:12 +0300

    Fix reviews component

--- src/core/services/api/reviews.service.ts
+++ src/core/services/api/reviews.service.ts
@@ -37,8 +37,12 @@ export class ReviewsService extends ApiService {
         );
     }
 
-    getReviews(orgId: any, orgType: OrganizationType, query?: any): Observable<any> {
+    getReviews(orgId: number = this.orgId, orgType: OrganizationType = this.orgType, query?: any): Observable<any> {
         const params = this.serializeParams(query);
         return this.post(this.orgPostUrl, `general/${orgType}/${orgId}/reviews?${params}`, 'GET');
+    }
+
+    getReviewsSummary(orgId: number = this.orgId, orgType: OrganizationType = this.orgType) {
+        return this.post(this.orgPostUrl, `general/${orgType}/${orgId}/reviews-summary`);
     }
 }
